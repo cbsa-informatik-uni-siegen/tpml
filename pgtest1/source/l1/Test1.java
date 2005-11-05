90cd28f2d9e4cc81943b446666bbdba91024e473
Benedikt Meurer <benedikt.meurer@googlemail.com>
2005-11-05 15:43:09 +0000

    Parser - generiert aus einer SableCC LALR(1) Grammatik - importiert und eine Translator Klasse, die den durch den Parser erzeugten AST in einen Smallstep Ausdruck umwandelt. Für Parser und die Umwandlung eine einfache Testklasse hinzugefügt.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@13 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest1/source/l1/Test1.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+
+package l1;
+
+import java.io.*;
+import java.util.ListIterator;
+
+import smallstep.Exn;
+import smallstep.Expression;
+import smallstep.Rule;
+import smallstep.RuleChain;
+import smallstep.Value;
+import l1.lexer.*;
+import l1.node.*;
+import l1.parser.*;
+
+/**
+ * Simple test class for the parser translator logic.
+ *
+ * @author bmeurer
+ * @version $Id$
+ */
+public class Test1 {
+  private static final String SIMPLE = "let f = lambda x.(if = x 0 then + else -) x 1 in f (f 9)";
+  
+  private static void evaluate(Expression e) {
+    System.out.println(e);
+    while (!(e instanceof Value) && !(e instanceof Exn)) {
+      RuleChain ruleChain = new RuleChain();
+      e = e.evaluate(ruleChain);
+      
+      if (ruleChain.isEmpty())
+        break;
+      
+      System.out.print(" -> " + e + " [");
+      
+      ListIterator<Rule> it = ruleChain.listIterator();
+      while (it.hasNext()) {
+        if (it.hasPrevious())
+          System.out.print(" ");
+        System.out.print("(" + it.next() + ")");
+      }
+      System.out.println("]");
+    }
+    System.out.println();
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    try {
+      // Allocate the parser
+      Parser parser = new Parser(new Lexer(new PushbackReader(new StringReader(SIMPLE), 1024)));
+      
+      // Parse the input
+      Start tree = parser.parse();
+      
+      // translate the AST to a small step expression
+      Translator translator = new Translator();
+      tree.apply(translator);
+      
+      // evaluate the resulting small step expression
+      evaluate(translator.getExpression());
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
---
+++ pgtest1/source/l1/Translator.java
@@ -0,0 +1,196 @@
+/**
+ * 
+ */
+package l1;
+
+import java.util.Stack;
+import l1.analysis.*;
+import l1.node.*;
+import smallstep.*;
+
+/**
+ * Used to translate an abstract syntax tree as produced
+ * by the SableCC generated parser into an expression
+ * required for the evaluation in the small step interpreter.
+ *
+ * @author bmeurer
+ * @version $Id$
+ */
+public class Translator extends DepthFirstAdapter {
+  /**
+   * Allocates a new translator.
+   */
+  public Translator() {
+  }
+  
+  /**
+   * Returns the parsed small step expression.
+   * @return the parsed small step expression.
+   */
+  public final Expression getExpression() {
+    assert (this.expressions.size() == 1);
+    return this.expressions.get(0);
+  }
+  
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAFalseExpression(l1.node.AFalseExpression)
+   */
+  @Override
+  public void outAFalseExpression(AFalseExpression node) {
+    this.expressions.push(BooleanConstant.FALSE);
+  }
+  
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outATrueExpression(l1.node.ATrueExpression)
+   */
+  @Override
+  public void outATrueExpression(ATrueExpression node) {
+    this.expressions.push(BooleanConstant.TRUE);
+  }
+  
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAAbstractionExpression(l1.node.AAbstractionExpression)
+   */
+  @Override
+  public void outAAbstractionExpression(AAbstractionExpression node) {
+    Expression e = this.expressions.pop();
+    this.expressions.push(new Abstraction(node.getId().getText(), e));
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAApplicationExpression(l1.node.AApplicationExpression)
+   */
+  @Override
+  public void outAApplicationExpression(AApplicationExpression node) {
+    Expression e2 = this.expressions.pop();
+    Expression e1 = this.expressions.pop();
+    this.expressions.push(new Application(e1, e2));
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAConditionExpression(l1.node.AConditionExpression)
+   */
+  @Override
+  public void outAConditionExpression(AConditionExpression node) {
+    Expression e2 = this.expressions.pop();
+    Expression e1 = this.expressions.pop();
+    Expression e0 = this.expressions.pop();
+    this.expressions.push(new Condition(e0, e1, e2));
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outADivideExpression(l1.node.ADivideExpression)
+   */
+  @Override
+  public void outADivideExpression(ADivideExpression node) {
+    this.expressions.push(ArithmeticOperator.DIV);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAEqualExpression(l1.node.AEqualExpression)
+   */
+  @Override
+  public void outAEqualExpression(AEqualExpression node) {
+    this.expressions.push(RelationalOperator.EQUALS);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAGreaterEqualExpression(l1.node.AGreaterEqualExpression)
+   */
+  @Override
+  public void outAGreaterEqualExpression(AGreaterEqualExpression node) {
+    this.expressions.push(RelationalOperator.GREATER_EQUAL);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAGreaterThanExpression(l1.node.AGreaterThanExpression)
+   */
+  @Override
+  public void outAGreaterThanExpression(AGreaterThanExpression node) {
+    this.expressions.push(RelationalOperator.GREATER_THAN);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAIdentifierExpression(l1.node.AIdentifierExpression)
+   */
+  @Override
+  public void outAIdentifierExpression(AIdentifierExpression node) {
+    this.expressions.push(new Identifier(node.getIdentifier().getText()));
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outALetExpression(l1.node.ALetExpression)
+   */
+  @Override
+  public void outALetExpression(ALetExpression node) {
+    Expression e1 = this.expressions.pop();
+    Expression e0 = this.expressions.pop();
+    this.expressions.push(new Let(node.getId().getText(), e0, e1));
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outALowerEqualExpression(l1.node.ALowerEqualExpression)
+   */
+  @Override
+  public void outALowerEqualExpression(ALowerEqualExpression node) {
+    this.expressions.push(RelationalOperator.LOWER_EQUAL);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outALowerThanExpression(l1.node.ALowerThanExpression)
+   */
+  @Override
+  public void outALowerThanExpression(ALowerThanExpression node) {
+    this.expressions.push(RelationalOperator.LOWER_THAN);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAMinusExpression(l1.node.AMinusExpression)
+   */
+  @Override
+  public void outAMinusExpression(AMinusExpression node) {
+    this.expressions.push(ArithmeticOperator.MINUS);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAModuloExpression(l1.node.AModuloExpression)
+   */
+  @Override
+  public void outAModuloExpression(AModuloExpression node) {
+    this.expressions.push(ArithmeticOperator.MOD);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAMultiplyExpression(l1.node.AMultiplyExpression)
+   */
+  @Override
+  public void outAMultiplyExpression(AMultiplyExpression node) {
+    this.expressions.push(ArithmeticOperator.MULT);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outANumberExpression(l1.node.ANumberExpression)
+   */
+  @Override
+  public void outANumberExpression(ANumberExpression node) {
+    this.expressions.push(new IntegerConstant(Integer.valueOf(node.getNumber().getText())));
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAPlusExpression(l1.node.APlusExpression)
+   */
+  @Override
+  public void outAPlusExpression(APlusExpression node) {
+    this.expressions.push(ArithmeticOperator.PLUS);
+  }
+
+  /**
+   * @see l1.analysis.DepthFirstAdapter#outAUnitExpression(l1.node.AUnitExpression)
+   */
+  @Override
+  public void outAUnitExpression(AUnitExpression node) {
+    this.expressions.push(UnitConstant.UNIT);
+  }
+  
+  private Stack<Expression> expressions = new Stack<Expression>();
+}
