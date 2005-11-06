260891a1b10c0280c4d0cce7266f6824f75a8f9f
Benedikt Meurer <benedikt.meurer@googlemail.com>
2005-11-06 17:40:12 +0000

    MLStyledDocument Syntax-Highlighting über den Lexer gesteuert.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@16 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest1/source/ui/MLStyledDocument.java
+++ pgtest1/source/ui/MLStyledDocument.java
@@ -1,132 +1,130 @@
 package ui;
 
 import java.awt.Color;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
 import javax.swing.text.*;
 
+import l1.lexer.*;
+import l1.node.*;
+
 public class MLStyledDocument extends DefaultStyledDocument {
+  private SimpleAttributeSet errorSet = new SimpleAttributeSet(); 
+  private SimpleAttributeSet normalSet = new SimpleAttributeSet(); 
+  private HashMap<String, SimpleAttributeSet> attributes = new HashMap<String, SimpleAttributeSet>();
+  private static final long serialVersionUID = -1779640687489585648L;
+  
+  public MLStyledDocument() {
+    // setup the error attribute set
+    StyleConstants.setForeground(this.errorSet, Color.RED);
+    StyleConstants.setUnderline(this.errorSet, true);
+    
+    // setup the normal attribute set
+    StyleConstants.setForeground(this.normalSet, Color.BLACK);
+    StyleConstants.setBold(this.normalSet, false);
 
-	private SimpleAttributeSet normal;
-	private SimpleAttributeSet keyword;
-	
-	public MLStyledDocument ()
-	{
-		Color keywordColor = new Color (0.6f, 0.0f, 0.0f);
-
-		normal = new SimpleAttributeSet ();
-		StyleConstants.setForeground (normal, Color.black);
-		StyleConstants.setBold (normal, false);
-		
-		keyword = new SimpleAttributeSet ();
-		StyleConstants.setForeground (keyword, keywordColor);
-		StyleConstants.setBold (keyword, true);
-		
-		System.out.println ("MLStyledDocument");
-	}
-	
-	public void insertString (int offset, String str, AttributeSet set)
-	throws BadLocationException
-	{
-		super.insertString (offset, str, set);
-		processChanged (offset, str.length ());
-	}
-	
-	public void remove (int offset, int length)
-	throws BadLocationException
-	{
-		super.remove (offset, length);
-		processChanged (offset, length);
-	}
-	
-	public void processChanged (int offset, int length)
-	throws BadLocationException
-	{
-		String content = getText(0, getLength());
-		 
-		Element root = getDefaultRootElement();
-		int startLine = root.getElementIndex( offset );
-		int endLine = root.getElementIndex( offset + length );
-
-		for (int i = startLine; i <= endLine; i++)
-		{
-			int startOffset = root.getElement( i ).getStartOffset();
-			int endOffset = root.getElement( i ).getEndOffset();
-			applyHighlighting(content, startOffset, endOffset);
-		}
-	}
-	
-	public void applyHighlighting (String content, int offsetStart, 
-			int offsetEnd)
-	throws BadLocationException
-	{
-		if (offsetEnd > content.length ())
-			offsetEnd = content.length ();
-		
-		int sequenceLength = offsetEnd - offsetStart;
-		setCharacterAttributes (offsetStart, sequenceLength, normal, true);
-		
-		int startIndex = offsetStart;
-		int endIndex = offsetStart;
-		String checkString = "";
-		
-		while (endIndex < offsetEnd)
-		{
-			char ch = content.charAt (endIndex);
-			if (isSplitter (ch))
-			{
-				if (isKeyword (checkString))
-					setCharacterAttributes (
-							endIndex - checkString.length(),
-							checkString.length(), keyword, true);
-				++endIndex;				
-				startIndex = endIndex;
-				checkString = "";
-			}
-			else
-			{
-				checkString += ch;
-				endIndex++;
-			}
-		}
-		if (isKeyword (checkString))
-		{
-			setCharacterAttributes (
-					endIndex - checkString.length(),
-					checkString.length (), keyword, true);
-		}
-		/*
-		//--endIndex;
-		String subString = "";
-		try {
-			subString = content.substring (startIndex, endIndex);
-		}
-		catch (Exception e)
-		{
-		}
-		if (isKeyword (subString))
-			setCharacterAttributes (startIndex,	endIndex-startIndex, 
-					keyword, true);
-		*/
-		
-	}
-	
-	boolean isSplitter (char character)
-	{
-		String splitCharacter = "+-*/=(),.:;<>{}[]";
-		return (Character.isWhitespace (character) ||
-				splitCharacter.indexOf (character) != -1);
-	}
-	boolean isKeyword (String word)
-	{
-		return (word.equals ("let")
-			|| word.equals ("in")
-			|| word.equals ("if")
-			|| word.equals ("then")
-			|| word.equals ("else")
-			|| word.equals ("int")
-			|| word.equals ("bool")
-			|| word.equals ("unit")
-			|| word.equals ("true")
-			|| word.equals ("false")
-			|| word.equals ("lambda"));
-	}
+    // setup the keyword set
+    SimpleAttributeSet keywordSet = new SimpleAttributeSet();
+    StyleConstants.setForeground(keywordSet, new Color(0.6f, 0.0f, 0.0f));
+    StyleConstants.setBold(keywordSet, true);
+    
+    this.attributes.put("Lambda", keywordSet);
+    this.attributes.put("Let", keywordSet);
+    this.attributes.put("In", keywordSet);
+    this.attributes.put("If", keywordSet);
+    this.attributes.put("Then", keywordSet);
+    this.attributes.put("Else", keywordSet);
+    
+    // setup the value set
+    SimpleAttributeSet valueSet = new SimpleAttributeSet();
+    StyleConstants.setForeground(valueSet, new Color(0.0f, 0.0f, 0.6f));
+    StyleConstants.setBold(valueSet, true);
+    
+    this.attributes.put("False", valueSet);
+    this.attributes.put("Number", valueSet);
+    this.attributes.put("True", valueSet);
+    this.attributes.put("Unit", valueSet);
+  }
+  
+  public void insertString (int offset, String str, AttributeSet set)
+      throws BadLocationException {
+    super.insertString (offset, str, set);
+    processChanged (offset, str.length ());
+  }
+  
+  public void remove (int offset, int length)
+      throws BadLocationException {
+    super.remove (offset, length);
+    processChanged (offset, length);
+  }
+  
+  public void processChanged (int offset, int length)
+      throws BadLocationException {
+    String content = getText(0, getLength());
+    
+    Element root = getDefaultRootElement();
+    int startLine = root.getElementIndex( offset );
+    int endLine = root.getElementIndex( offset + length );
+    
+    for (int i = startLine; i <= endLine; i++)
+    {
+      int startOffset = root.getElement( i ).getStartOffset();
+      int endOffset = root.getElement( i ).getEndOffset();
+      applyHighlighting(content, startOffset, endOffset);
+    }
+  }
+  
+  private void applyHighlighting (String content, int offsetStart, int offsetEnd)
+      throws BadLocationException {
+    StringReader reader = new StringReader(content.substring(offsetStart, offsetEnd - 1));
+    Lexer lexer = new Lexer(new PushbackReader(reader, 1024));
+    
+    try {
+      for (;;) {
+        try {
+          Token token = lexer.next();
+          if (token instanceof EOF)
+            break;
+          
+          // determine the token name from the token class
+          String tokenName = token.getClass().getSimpleName().substring(1);
+          
+          // check if we have an attribute set for the token
+          SimpleAttributeSet set = this.attributes.get(tokenName);
+          
+          // fallback to the normal attribute set
+          if (set == null)
+            set = this.normalSet;
+          
+          // apply the character attribute set
+          setCharacterAttributes(offsetStart + token.getPos() - 1, token.getText().length(), set, true);
+        }
+        catch (LexerException e) {
+          // Parse the lexer exception text: "[<line>,<col>] Unknown token: <token>"
+          Pattern pattern = Pattern.compile("^\\[(\\d)+,(\\d+)\\] Unknown token: (.+)$");
+          Matcher matcher = pattern.matcher(e.getMessage());
+          if (!matcher.find())
+            throw e;
+          
+          // extract position and token
+          int pos = Integer.valueOf(matcher.group(2));
+          String token = matcher.group(3);
+          
+          // apply the error character set
+          setCharacterAttributes(offsetStart + pos - 1, token.length(), this.errorSet, true);
+          
+          // setup the lexer to parse the remaining input
+          offsetStart += pos + token.length() - 1;
+          if (offsetStart >= offsetEnd)
+            break;
+          reader = new StringReader(content.substring(offsetStart, offsetEnd - 1));
+          lexer = new Lexer(new PushbackReader(reader, 1024));
+        }
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
--- pgtest1/source/ui/SourceFile.java
+++ pgtest1/source/ui/SourceFile.java
@@ -5,46 +5,41 @@ import java.awt.Component;
 import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
 import javax.swing.text.StyledEditorKit;
+import java.awt.event.*;
 
 public class SourceFile {
-
-	static private int num = 0;
-	private JScrollPane scrollPane;
-	private JEditorPane editorPane;
-	private String filename;
-
-	
-	public SourceFile ()
-	{
-		filename = "newfile" + num + ".ml";
-		num++;
-		scrollPane = new JScrollPane ();
-		editorPane =  new JEditorPane ();
-		editorPane.setName (filename);
-		editorPane.setAutoscrolls (false);
-		editorPane.setEditorKit (new StyledEditorKit ());
-		editorPane.setDocument (new MLStyledDocument ());
-		scrollPane.setViewportView (editorPane);
-		scrollPane.setName (filename);
-	}
-	
-	public void setName (String name)
-	{
-		filename = name;
-		scrollPane.setName (filename);
-	}
-	
-	public String getFilename ()
-	{
-		return (filename);
-	}
-	
-	public Component getComponent ()
-	{
-		return (scrollPane);
-	}
-
-	public MLStyledDocument getDocument() {
-		return (MLStyledDocument)editorPane.getDocument();
-	}
+  static private int num = 0;
+  private JScrollPane scrollPane;
+  private JEditorPane editorPane;
+  private String filename;
+  
+  public SourceFile() {
+    this.filename = "newfile" + num + ".ml";
+    num++;
+    this.scrollPane = new JScrollPane ();
+    this.editorPane =  new JEditorPane ();
+    this.editorPane.setName (filename);
+    this.editorPane.setAutoscrolls (false);
+    this.editorPane.setEditorKit (new StyledEditorKit ());
+    this.editorPane.setDocument (new MLStyledDocument ());
+    this.scrollPane.setViewportView (editorPane);
+    this.scrollPane.setName (filename);
+  }
+  
+  public void setName(String name) {
+    this.filename = name;
+    this.scrollPane.setName(this.filename);
+  }
+  
+  public String getFilename() {
+    return this.filename;
+  }
+  
+  public Component getComponent() {
+    return this.scrollPane;
+  }
+  
+  public MLStyledDocument getDocument() {
+    return (MLStyledDocument)editorPane.getDocument();
+  }
 }
