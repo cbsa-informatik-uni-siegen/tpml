4e18279ebd38d8726d9782486018d34865b16e69
Benedikt Meurer <benedikt.meurer@googlemail.com>
2006-04-30 14:00:28 +0000

    Gerüst für SmallStep Interpreter.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@127 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest1/source/smallstep/SmallStepProofModel.java
@@ -0,0 +1,13 @@
+package smallstep;
+
+import common.ProofModel;
+
+/**
+ * TODO Add documentation here.
+ *
+ * @author Benedikt Meurer
+ * @version $Id$
+ */
+public class SmallStepProofModel extends ProofModel {
+
+}
---
+++ pgtest1/source/smallstep/SmallStepProofNode.java
@@ -0,0 +1,13 @@
+package smallstep;
+
+import common.ProofNode;
+
+/**
+ * TODO Add documentation here.
+ *
+ * @author Benedikt Meurer
+ * @version $Id$
+ */
+public class SmallStepProofNode extends ProofNode {
+
+}
