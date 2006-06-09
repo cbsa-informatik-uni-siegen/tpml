a714de31b693b6326c6f596b04458977f1aa4542
Christoph Fehling <christoph.fehling@gmx.de>
2006-06-09 10:26:48 +0000

    hatte vergessen zwei dateien hinzuzufügen
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@166 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest1/source/ui/newgui/EditorComponent.java
@@ -0,0 +1,30 @@
+package ui.newgui;
+
+import java.util.List;
+import java.awt.Component;
+
+
+/**
+ * TODO Add documentation here.
+ *
+ * @author Benedikt Meurer
+ * @version $Id: EditorPane.java 137 2006-05-09 17:51:32Z benny $
+ */
+public interface EditorComponent {  
+  /**
+   * Returns the {@link DefaultEditorAction}s for this editor pane.
+   * 
+   * @return the {@link DefaultEditorAction}s.
+   */
+  public List<EditorAction> getActions();
+  
+  /**
+   * Returns the title for this editor pane, i.e. <tt>"Source"</tt>
+   * in case of the source pane.
+   * 
+   * @return the title for this editor pane.
+   */
+  public String getTitle();
+  
+  public Component getDisplay();
+}
