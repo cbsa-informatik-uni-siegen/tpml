e42edf3ae18335e6a4f9063ec9899293ccd07791
Benedikt Meurer <benedikt.meurer@googlemail.com>
2006-06-15 14:12:29 +0000

    Bean-Kram für EditorAction's.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@172 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest1/source/ui/newgui/DefaultEditorAction.java
+++ pgtest1/source/ui/newgui/DefaultEditorAction.java
@@ -1,9 +1,10 @@
 package ui.newgui;
 
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
 
 import javax.swing.Icon;
+
+import common.beans.AbstractBean;
 
 /**
  * TODO Add documentation here.
@@ -11,51 +12,56 @@ import javax.swing.Icon;
  * @author Benedikt Meurer
  * @version $Id: EditorAction.java 137 2006-05-09 17:51:32Z benny $
  */
-public class DefaultEditorAction implements EditorAction{
-	private String title;
-	private int group;
-	private Icon icon;
-	private boolean enabled;
-	private ActionListener actionlistener;
-  public ActionListener getActionlistener() {
-		return actionlistener;
-	}
-
-	public void setActionlistener(ActionListener actionlistener) {
-		this.actionlistener = actionlistener;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	public void setGroup(int group) {
-		this.group = group;
-	}
-
-	public void setIcon(Icon icon) {
-		this.icon = icon;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-/**
-   * Adds a <code>PropertyChangeListener</code> for the properties
-   * of this action.
-   * 
-   * @param listener the <code>PropertyChangeListener</code> to add.
-   */
-  public void addPropertyChangeListener(PropertyChangeListener listener){};
+public class DefaultEditorAction extends AbstractBean implements EditorAction {
+  //
+  // Attributes
+  //
   
-  /**
-   * Removes a <code>PropertyChangeListener</code> for the properties
-   * of this action.
-   *  
-   * @param listener the <code>PropertyChangeListener</code> to remove.
-   */
-  public void removePropertyChangeListener(PropertyChangeListener listener){};
+  private String title;
+  private int group;
+  private Icon icon;
+  private boolean enabled;
+  private ActionListener actionlistener;
+  
+
+  
+  //
+  // Primitives
+  //
+  
+  public ActionListener getActionlistener() {
+    return actionlistener;
+  }
+  
+  public void setActionlistener(ActionListener actionlistener) {
+    ActionListener oldActionlistener = this.actionlistener;
+    this.actionlistener = actionlistener;
+    firePropertyChange("actionlistener", oldActionlistener, actionlistener);
+  }
+  
+  public void setEnabled(boolean enabled) {
+    boolean oldEnabled = this.enabled;
+    this.enabled = enabled;
+    firePropertyChange("enabled", oldEnabled, enabled);
+  }
+  
+  public void setGroup(int group) {
+    int oldGroup = this.group; 
+    this.group = group;
+    firePropertyChange("group", oldGroup, group);
+  }
+  
+  public void setIcon(Icon icon) {
+    Icon oldIcon = this.icon;
+    this.icon = icon;
+    firePropertyChange("icon", oldIcon, icon);
+  }
+  
+  public void setTitle(String title) {
+    String oldTitle = this.title;
+    this.title = title;
+    firePropertyChange("title", oldTitle, title);
+  }
   
   /**
    * Returns <code>true</code> if the action can be performed, that is,
@@ -64,9 +70,9 @@ public class DefaultEditorAction implements EditorAction{
    * @return <code>true</code> if enabled.
    */
   public boolean isEnabled(){
-	  return enabled;
+    return enabled;
   };
-
+  
   /**
    * Returns the grouping id of this action. Actions with similar group
    * ids should be displayed together, while spacing should be inserted
@@ -82,7 +88,7 @@ public class DefaultEditorAction implements EditorAction{
    * @return the {@link Icon} for this action.
    */
   public Icon getIcon(){
-	  return icon;
+    return icon;
   };
   
   /**
@@ -91,8 +97,8 @@ public class DefaultEditorAction implements EditorAction{
    * @return the title (or tooltip) for this action.
    */
   public String getTitle(){return title;}
-
-public ActionListener getActionListener() {
-	return actionlistener;
-};
+  
+  public ActionListener getActionListener() {
+    return actionlistener;
+  };
 }
