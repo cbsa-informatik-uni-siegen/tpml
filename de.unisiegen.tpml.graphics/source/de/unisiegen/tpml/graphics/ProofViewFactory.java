54025c62eac1819d9ec8e947af86ff135f21b399
Marcell Fischbach <marcellfischbach@googlemail.com>
2006-09-25 15:20:30 +0000

    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@302 69bfd797-8304-0410-886d-9c04947c740f

--- de.unisiegen.tpml.graphics/source/de/unisiegen/tpml/graphics/ProofViewFactory.java
+++ de.unisiegen.tpml.graphics/source/de/unisiegen/tpml/graphics/ProofViewFactory.java
@@ -1,12 +1,17 @@
 package de.unisiegen.tpml.graphics;
 
+import javax.swing.JComboBox;
+
 import de.unisiegen.tpml.core.typechecker.TypeCheckerProofModel;
+import de.unisiegen.tpml.graphics.renderer.AbstractRenderer;
+import de.unisiegen.tpml.graphics.theme.Theme;
 import de.unisiegen.tpml.graphics.typechecker.TypeCheckerView;
 
 public class ProofViewFactory {
 
 	
 	public static ProofView newTypeCheckerView (TypeCheckerProofModel model) {
+		AbstractRenderer.setTheme(new Theme (), new JComboBox());
 		return (ProofView) new TypeCheckerView (model);
 	}
 	
