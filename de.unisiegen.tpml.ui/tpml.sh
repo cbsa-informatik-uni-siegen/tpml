840ba3fbf0c9e28451e550859859c20dca98f1d9
Benedikt Meurer <benedikt.meurer@googlemail.com>
2006-10-06 14:53:44 +0000

    Add start script for Unix/Linux. Generate both zip and tar.bz2 archives.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@365 69bfd797-8304-0410-886d-9c04947c740f

---
+++ de.unisiegen.tpml.ui/tpml.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Startscript for the TPML application.
+#
+
+# Check if a JRE is available
+if ! type java >/dev/null 2>&1; then
+	cat >&2 <<EOF
+No suitable JRE found. Please download and install a JRE and make sure the
+java binary is in the PATH.
+EOF
+	exit 1
+fi
+
+exec java -jar de.unisiegen.tpml.ui-@de.unisiegen.tpml.ui.version@.jar
