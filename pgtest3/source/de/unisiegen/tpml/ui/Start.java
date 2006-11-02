9230e56dd3592c45439b617fc358540f06203c53
Christian Fehler <christian.fehler@gmx.de>
2006-11-02 15:27:02 +0000

    pgtest3 created
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@537 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest3/source/de/unisiegen/tpml/ui/Start.java
@@ -0,0 +1,17 @@
+package de.unisiegen.tpml.ui;
+
+
+/**
+ * TODO
+ */
+public final class Start {
+	/**
+     * TODO
+     * 
+	 * @param args TODO
+	 */
+	public static void main(final String args[]) 
+    {
+	  Main.main ( args ) ;	
+	}
+}
