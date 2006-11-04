d30583f394961285b72cb617b4834a99f455305f
Christian Fehler <christian.fehler@gmx.de>
2006-11-04 17:36:10 +0000

    javadoc completed
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@544 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/ui/Start.java
+++ pgtest3/source/de/unisiegen/tpml/ui/Start.java
@@ -1,17 +1,21 @@
-package de.unisiegen.tpml.ui;
+package de.unisiegen.tpml.ui ;
 
 
 /**
- * TODO
+ * The main starter class for the pgtest3 project.
+ * 
+ * @author Christian Fehler
  */
-public final class Start {
-	/**
-     * TODO
-     * 
-	 * @param args TODO
-	 */
-	public static void main(final String args[]) 
-    {
-	  Main.main ( args ) ;	
-	}
+public final class Start
+{
+  /**
+   * The main entry point for the pgtest3 project. This method starts the TPML
+   * project.
+   * 
+   * @param args the command line arguments.
+   */
+  public static void main ( final String args[] )
+  {
+    Main.main ( args ) ;
+  }
 }
