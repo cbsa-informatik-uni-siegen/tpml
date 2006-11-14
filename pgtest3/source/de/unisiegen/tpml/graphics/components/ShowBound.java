52cbb1f0e3e03e7c6fdac7ecb4e5e8b49458e8d6
Benjamin Mies <benjamin.mies@googlemail.com>
2006-11-14 20:27:25 +0000

    Fixed Version lambda, let all right, even should be ;-)
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@613 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/graphics/components/ShowBound.java
+++ pgtest3/source/de/unisiegen/tpml/graphics/components/ShowBound.java
@@ -2,9 +2,7 @@ package de.unisiegen.tpml.graphics.components;
 
 import java.util.Enumeration;
 import java.util.LinkedList;
-import java.util.Vector;
 
-import de.unisiegen.tpml.core.expressions.Application;
 import de.unisiegen.tpml.core.expressions.Expression;
 import de.unisiegen.tpml.core.expressions.Identifier;
 import de.unisiegen.tpml.core.expressions.Lambda;
@@ -12,13 +10,13 @@ import de.unisiegen.tpml.core.expressions.Let;
 import de.unisiegen.tpml.core.expressions.MultiLambda;
 import de.unisiegen.tpml.core.prettyprinter.PrettyAnnotation;
 import de.unisiegen.tpml.core.prettyprinter.PrettyString;
+import de.unisiegen.tpml.graphics.components.Bound;
 
 public class ShowBound
 {
+static Expression holeExpression;
+	
 
-	
-	static Expression holeExpression;
-	
 	private static ShowBound bound = null;
 	private static LinkedList<Bound> tmp = new LinkedList();
 	public LinkedList<Bound> result = new LinkedList();
@@ -36,26 +34,27 @@ public class ShowBound
 			return bound;
 		}
 	}
-
+	
+	
 	public void check(Expression pExpression)
 	{
-
+	
 		{
 
 			if (pExpression instanceof Lambda)
 			{
 
-				checkBoundLambda((Lambda) pExpression);
+				checkLambda((Lambda) pExpression);
 			}
 			else if (pExpression instanceof Let)
 			{
 
-				checkBoundLet((Let) pExpression);
+				checkLet((Let) pExpression);
 			}
 			else if (pExpression instanceof MultiLambda)
 			{
 
-				checkBoundMultiLambda((MultiLambda) pExpression);
+				checkMultiLambda((MultiLambda) pExpression);
 			}
 			else
 			{
@@ -73,78 +72,63 @@ public class ShowBound
 		}
 
 	}
-
-	public void checkBoundLambda(Lambda pLambda)
-	{
-		
-		Expression e = pLambda.getE();
-
-		check(e);
-
-		Object[] a = pLambda.free().toArray();
-		Object[] b = e.free().toArray();
-
-		System.out.println(pLambda.toString());
-		checkBound(pLambda, a, b);
-		
-	}
-
-	public void checkBoundLet(Let pLet)
-	{
-		Object[] a = pLet.getE1().free().toArray();
-		Object[] b = pLet.getE2().free().toArray();
-		
-		//System.out.println(pLet.toString());
-		//System.out.println(pLet.getE1().toString());
-		//System.out.println(pLet.getE2().toString());
-		checkBound(pLet,a,b);
-	}
 	
-	public void checkBoundMultiLambda(MultiLambda pMultiLambda)
+	private void checkLambda(Lambda lambda)
 	{
-		Expression e = pMultiLambda.getE();
-
+		// rekursiver Aufruf für Ausdruck e von lambda
+		Expression e = lambda.getE();
 		check(e);
-
-		Object[] a = pMultiLambda.free().toArray();
+		
+		// anlegen von Arrays mit den frei vorkommenden Namen der beiden expressions von lambda
+		Object[] a = lambda.free().toArray();
 		Object[] b = e.free().toArray();
 		
-		checkBound(pMultiLambda,a,b);
-	}
-
-	public void checkBound (Expression pE, Object[] a, Object[] b)
-	{
-		LinkedList<String> e1 = new LinkedList();
-		LinkedList<String> e2 = new LinkedList();
+		LinkedList list = listWithBounds(a, b);
 		
-		for (int i = 0; i < a.length; i++)
-		{
-
-			e1.add((String) a[i]);
-		}
-		for (int j = 0; j < b.length; j++)
-		{
-
-			e2.add((String) b[j]);
-		}
-
-		for (int i = 0; i < e1.size(); i++)
-		{
-			if (e2.contains(e1.get(i)))
-			{
-				e2.remove(i);
-				i--;
-			}
-		}
-
-		childCheck(pE.children(), e2, pE);
+	checkRec(lambda.children(),lambda,list);
+	
 
 	}
 	
-	public void childCheck(Enumeration child, LinkedList e2, Expression pE)
+	
+	private void checkMultiLambda(MultiLambda lambda)
 	{
-			
+		// rekursiver Aufruf für Ausdruck e von lambda
+		Expression e = lambda.getE();
+		check(e);
+		
+		// anlegen von Arrays mit den frei vorkommenden Namen der beiden expressions von lambda
+		Object[] a = lambda.free().toArray();
+		Object[] b = e.free().toArray();
+		
+		LinkedList list = listWithBounds(a, b);
+		
+checkRec(lambda.children(),lambda,list);
+	
+		
+	}
 
+
+	private void checkLet(Let let)
+	{
+		// anlegen von Arrays mit den frei vorkommenden Namen der beiden expressions von lambda
+		Object[] a = let.getE1().free().toArray();
+		Object[] b = let.getE2().free().toArray();
+		
+		LinkedList list = listWithBounds(a, b);
+		
+		
+		checkRec(let.children(),let,list);
+
+		
+	}
+
+	//TODO just working begin
+	private Bound checkRec (Enumeration child,Expression e, LinkedList <String> list)
+	{
+		Bound result=null;
+		
+		
 		while (child.hasMoreElements())
 		{
 
@@ -158,36 +142,33 @@ public class ShowBound
 
 					Identifier id = (Identifier) actualExpression;
 				
-					for (int i = 0; i < e2.size(); i++)
+					for (int i = 0; i < list.size(); i++)
 					{
 						
-						if (id.getName().equals(e2.get(i)))
+						if (id.getName().equals(list.get(i)))
 						{
 							
 							PrettyString ps1 = holeExpression.toPrettyString();
-							PrettyAnnotation mark1 = ps1.getAnnotationForPrintable(pE);
+							PrettyAnnotation mark1 = ps1.getAnnotationForPrintable(e);
 							PrettyAnnotation mark2 = ps1.getAnnotationForPrintable(id);
 							
-							//TODO Just Testing
-							//System.out.println("Identifier:"+pE.toString());
-							//System.out.println("Var:"+id.toString());
 							
 							
 							
 							int start =0;
-							if (holeExpression instanceof Lambda)
+							if (e instanceof Lambda)
 							{
 								start = mark1.getStartOffset() + 1;
 								
 							}
 							else 
-								if (holeExpression instanceof Let)
+								if (e instanceof Let)
 							{
 								start = mark1.getStartOffset() + 4;
 							}
 							
 							int length = start + id.toString().length()-1;
-
+						
 							System.err.println("Für den Identifier: Startoffset: " + start
 									+ "Endoffset" + length);
 							System.err.println("Für die Variable " + id.getName()
@@ -196,55 +177,69 @@ public class ShowBound
 							Bound addToList = new Bound(start,length);
 							addToList.getMarks().add(mark2);
 							tmp.add(addToList);
+					
 							
-							//TODO just Testing
-							getAnnotations();
+							
+					
+							
+							
 							
 						}
 					}
 
 				}
 				else
-					childCheck(actualExpression.children(), e2, pE);
+					checkRec(actualExpression.children(),e, list);
 			}
 
 		}
+		
+		return result;
+	}
+	//TODO just working end
+	
+	
+	public LinkedList<String> listWithBounds ( Object[] a, Object[] b)
+	{
+		LinkedList<String> e1 = new LinkedList();
+		LinkedList<String> e2 = new LinkedList();
+		
+		e1=castArray(a);
+		e2=castArray(b);
+
+		for (int i = 0; i < e1.size(); i++)
+		{
+			if (e2.contains(e1.get(i)))
+			{
+				e2.remove(i);
+				i--;
+			}
+		}
+		return e2;
 	}
 	
-
-	public LinkedList getAnnotations()
+	private LinkedList<String> castArray(Object[] a)
 	{
-		boolean found=false;
-		System.out.println("Tmpsize"+tmp.size());
+		LinkedList<String> tmp = new LinkedList();
 		
-		for (int i=0; i<tmp.size();i=i+2)
+		for (int i = 0; i < a.length; i++)
 		{
-			found=false;
-			for (int j=0; j<result.size();j++ )
-			{
-				if (tmp.get(i).getStartOffset()== result.get(j).getStartOffset())
-				{
-					result.get(j).getMarks().addAll(tmp.get(i).getMarks());
-					found=true;
-				}
-			}
-			if (!found)
-			{
-				Bound listIt = new Bound(tmp.get(i).getStartOffset(),tmp.get(i).getEndOffset());
-				listIt.getMarks().addAll(tmp.get(i).getMarks());
-				result.add(listIt);
-				
-			}
+
+			tmp.add((String) a[i]);
 		}
-		System.out.println("Array: "+result.size());
-		return result;
+		
+		return tmp;
 	}
 
 	public static void setHoleExpression(Expression holeExpression)
 	{
-		ShowBound.holeExpression = holeExpression;
+		bound.holeExpression = holeExpression;
 		tmp=new LinkedList();
 	}
+	public LinkedList getAnnotations()
+	{
+		return tmp;
+	}
+	
+
 }
-
-
