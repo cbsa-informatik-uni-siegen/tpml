efc14fa9f054ca97e79fd5b0167cba54c3900a5d
Benjamin Mies <benjamin.mies@googlemail.com>
2006-11-21 11:45:23 +0000

    Failure in LinkedList fixed
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@664 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/graphics/components/ShowBound.java
+++ pgtest3/source/de/unisiegen/tpml/graphics/components/ShowBound.java
@@ -7,6 +7,7 @@ import de.unisiegen.tpml.core.expressions.CurriedLet;
 import de.unisiegen.tpml.core.expressions.CurriedLetRec;
 import de.unisiegen.tpml.core.expressions.Expression;
 import de.unisiegen.tpml.core.expressions.Identifier;
+import de.unisiegen.tpml.core.expressions.InfixOperation;
 import de.unisiegen.tpml.core.expressions.Lambda;
 import de.unisiegen.tpml.core.expressions.Let;
 import de.unisiegen.tpml.core.expressions.LetRec;
@@ -23,7 +24,7 @@ public class ShowBound
 
 	private static ShowBound bound = null;
 	private static LinkedList<Bound> tmp = new LinkedList<Bound>();
-	private boolean debugOutput = true;
+	private boolean debugOutput = false;
 	// public LinkedList<Bound> result = new LinkedList();
 
 	public static ShowBound getInstance()
@@ -42,6 +43,7 @@ public class ShowBound
 
 	public void check(Expression pExpression)
 	{
+		
 
 		{
 
@@ -82,17 +84,7 @@ public class ShowBound
 			}
 			else
 			{
-				/**
-				  Enumeration<Expression> child = pExpression.children(); while
-				  (child.hasMoreElements()) {
-				  
-				  Expression actualExpression = (Expression) child.nextElement(); if
-				  (debugOutput) { System.err.println(actualExpression.toString());
-				   }
-				  
-				  check(actualExpression);
-				   }
-				 */
+				
 				
 				
 				LinkedList<Expression> child = new LinkedList<Expression>();
@@ -103,26 +95,32 @@ public class ShowBound
 					child.add((Expression) tmpChild.nextElement());
 
 				}
-				for (int j=1; j<child.size();j++)
-				{
-					if (child.get(j).hashCode()==child.get(j-1).hashCode())
-					{
-						System.out.println("2 gleiche Objekte");
-						System.out.println(child.get(j)+"Hash:"+child.get(j).hashCode()+" und "+child.get(j-1)+" Hashcode:"+child.get(j-1).hashCode());
-						
-					}
-				}
 				
+			
+				
+				{
 				for (int i = 0; i < child.size(); i++)
 			
 				{
-						
-						
-					
-					
 					check(child.get(i));
+					
+					if (debugOutput)
+					{
+					System.out.print(child.get(i).toPrettyString());
+					
+					try
+					{
+					PrettyString ps = holeExpression.toPrettyString();
+					PrettyAnnotation mark=ps.getAnnotationForPrintable(child.get(i));
+					System.out.println("start:"+mark.getStartOffset());
+					}
+					catch (Exception e)
+					{
+						System.out.println("Mist da!");
+					}
+					}
 				}
-				
+				}
 			}
 
 		}
@@ -207,7 +205,7 @@ public class ShowBound
 	{
 		// anlegen von Arrays mit den frei vorkommenden Namen der beiden expressions
 		// von lambda
-		Object[] a = new Object[0];
+		Object[] a = let.free().toArray();
 		Object[] b = let.getE2().free().toArray();
 
 		check(let.getE1());
@@ -221,33 +219,40 @@ public class ShowBound
 		{
 			child.add((Expression) tmpChild.nextElement());
 		}
-
+		child.remove(child.size()-1);
 		checkRec(child, let, list);
 
 	}
 
 	private void checkCurriedLet(CurriedLet let)
 	{
-		// anlegen von Arrays mit den frei vorkommenden Namen der beiden expressions
-		// von lambda
-
-		Object[] a = let.getE2().free().toArray();
+//	 anlegen von Arrays mit den frei vorkommenden Namen der beiden expressions
+		Object[] c = let.getE2().free().toArray();
 		Object[] b = let.getE1().free().toArray();
-		Object[] tmp = new Object[b.length];
-
-		for (int i = 0; i < b.length; i++)
-		{
-			if (!b[i].equals(let.getIdentifiers(0)))
-			{
-				tmp[i] = b[i];
-			}
-		}
+		Object[] a = let.free().toArray();
 
 		check(let.getE1());
 		check(let.getE2());
 
-		LinkedList <String> list = listWithBounds(new Object[0], tmp);
-		LinkedList <String> list2 = listWithBounds(tmp, a);
+		System.out.println("a");
+		for (int i=0; i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		System.out.println("b");
+		for (int i=0; i<b.length;i++)
+		{
+			System.out.println(b[i]);
+		}
+		System.out.println("c");
+		for (int i=0; i<c.length;i++)
+		{
+			System.out.println(c[i]);
+		}
+		
+
+		LinkedList <String>list = listWithBounds(c, a);
+		LinkedList<String> list2 = listWithBounds(c, b);
 
 		LinkedList<Expression> child = new LinkedList<Expression>();
 		Enumeration tmpChild = let.children();
@@ -257,9 +262,10 @@ public class ShowBound
 		}
 		LinkedList<Expression> child2 = new LinkedList<Expression>();
 		child2.add((child.getLast()));
-		checkRec(child2, let, list2);
+		checkRec(child2, let, list);
 		child.remove(child.size() - 1);
-		checkRec(child, let, list);
+		checkRec(child, let, list2);
+
 
 	}
 
@@ -290,21 +296,17 @@ public class ShowBound
 		// anlegen von Arrays mit den frei vorkommenden Namen der beiden expressions
 		Object[] a = rec.getE2().free().toArray();
 		Object[] b = rec.getE1().free().toArray();
-		Object[] tmp = new Object[b.length];
+		Object[] c = rec.free().toArray();
 
 		check(rec.getE1());
 		check(rec.getE2());
 
-		for (int i = 0; i < b.length; i++)
-		{
-			if (!b[i].equals(rec.getIdentifiers(0)))
-			{
-				tmp[i] = b[i];
-			}
-		}
+		
+		
+		
 
-		LinkedList <String>list = listWithBounds(new Object[0], tmp);
-		LinkedList<String> list2 = listWithBounds(tmp, a);
+		LinkedList <String>list = listWithBounds(c, a);
+		LinkedList<String> list2 = listWithBounds(c, b);
 
 		LinkedList<Expression> child = new LinkedList<Expression>();
 		Enumeration tmpChild = rec.children();
@@ -314,9 +316,9 @@ public class ShowBound
 		}
 		LinkedList<Expression> child2 = new LinkedList<Expression>();
 		child2.add((child.getLast()));
-		checkRec(child2, rec, list2);
+		checkRec(child2, rec, list);
 		child.remove(child.size() - 1);
-		checkRec(child, rec, list);
+		checkRec(child, rec, list2);
 
 	}
 
@@ -343,8 +345,7 @@ public class ShowBound
 
 						if (id.getName().equals(list.get(i)))
 						{
-							if (debugOutput)
-								System.out.println("checkRec:" + e.toString());
+					
 							
 							PrettyString ps1 = holeExpression.toPrettyString();
 							PrettyAnnotation mark1 = ps1.getAnnotationForPrintable(e);
@@ -354,7 +355,7 @@ public class ShowBound
 
 							int length = start + id.toString().length() - 1;
 
-							if (debugOutput)
+							if (false)
 							{
 								System.err.println("Für den Identifier: Startoffset: " + start
 										+ "Endoffset" + length);
@@ -362,9 +363,22 @@ public class ShowBound
 										+ " Startoffset: " + mark2.getStartOffset()
 										+ " Endoffset: " + mark2.getEndOffset());
 							}
-							Bound addToList = new Bound(start, length);
-							addToList.getMarks().add(mark2);
-							tmp.add(addToList);
+							boolean exists=false;
+							for (int k=0; k<tmp.size();k++)
+							{
+								if (start == tmp.get(k).getStartOffset())
+								{
+									exists=true;
+									tmp.get(k).getMarks().add(mark2);
+								}
+							}
+							if (!exists)
+							{
+								Bound addToList = new Bound(start, length);
+								addToList.getMarks().add(mark2);
+								tmp.add(addToList);
+							}
+							
 
 						}
 					}
@@ -516,6 +530,8 @@ public class ShowBound
 	public static void setHoleExpression(Expression holeExpression)
 	{
 		ShowBound.holeExpression = holeExpression;
+		
+		
 		tmp = new LinkedList<Bound>();
 	}
 
