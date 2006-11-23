438024b9278bcebcb2f4f52ed08f323df6308318
Christian Fehler <christian.fehler@gmx.de>
2006-11-23 14:47:50 +0000

    Add final strings for the users Christian, Michael and Benjamin
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@743 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/Debug.java
+++ pgtest3/source/de/unisiegen/tpml/Debug.java
@@ -3,6 +3,15 @@ package de.unisiegen.tpml ;
 
 public class Debug
 {
+  public final static String CHRISTIAN = "christian" ;
+
+
+  public final static String MICHAEL = "feivel" ;
+
+
+  public final static String BENJAMIN = "benjamin" ;
+
+
   public static DebugOutPrint out = new DebugOutPrint ( ) ;
 
 
@@ -16,15 +25,15 @@ public class Debug
   }
 
 
+  public static void removeMe ( )
+  {
+    removeUser ( System.getProperties ( ).getProperty ( "user.name" ) ) ;
+  }
+
+
   public static void removeUser ( String username )
   {
     out.remove ( username ) ;
     err.remove ( username ) ;
-  }
-
-
-  public static void removeMe ( )
-  {
-    removeUser ( System.getProperties ( ).getProperty ( "user.name" ) ) ;
   }
 }
