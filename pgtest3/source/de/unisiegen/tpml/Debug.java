5b321cbf1bd274145acfe822a57f4f27b83d78ab
Christian Fehler <christian.fehler@gmx.de>
2006-11-23 10:32:17 +0000

    Added support for other primitive data types like short, long, double ...
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@719 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/Debug.java
+++ pgtest3/source/de/unisiegen/tpml/Debug.java
@@ -1,7 +1,5 @@
 package de.unisiegen.tpml ;
 
-import java.util.Properties;
-
 
 public class Debug
 {
@@ -16,16 +14,17 @@ public class Debug
     out.getUserDebug ( ).add ( username ) ;
     err.getUserDebug ( ).add ( username ) ;
   }
-  public static void removeUser(String username)
+
+
+  public static void removeUser ( String username )
   {
-  	 out.remove ( username ) ;
-     err.remove ( username ) ;
+    out.remove ( username ) ;
+    err.remove ( username ) ;
   }
-  public static void removeMe()
+
+
+  public static void removeMe ( )
   {
-  	Properties prop = System.getProperties ( ) ;
-    String username= prop.getProperty ( "user.name" )  ;
-  	 out.remove ( username ) ;
-     err.remove ( username ) ;
+    removeUser ( System.getProperties ( ).getProperty ( "user.name" ) ) ;
   }
 }
--- pgtest3/source/de/unisiegen/tpml/DebugOutPrint.java
+++ pgtest3/source/de/unisiegen/tpml/DebugOutPrint.java
@@ -17,6 +17,48 @@ public class DebugOutPrint
   }
 
 
+  public LinkedList < String > getUserDebug ( )
+  {
+    return this.userDebug ;
+  }
+
+
+  public void print ( boolean toPrint , String username )
+  {
+    print ( String.valueOf ( toPrint ) , username ) ;
+  }
+
+
+  public void print ( double toPrint , String username )
+  {
+    print ( String.valueOf ( toPrint ) , username ) ;
+  }
+
+
+  public void print ( float toPrint , String username )
+  {
+    print ( String.valueOf ( toPrint ) , username ) ;
+  }
+
+
+  public void print ( int toPrint , String username )
+  {
+    print ( String.valueOf ( toPrint ) , username ) ;
+  }
+
+
+  public void print ( long toPrint , String username )
+  {
+    print ( String.valueOf ( toPrint ) , username ) ;
+  }
+
+
+  public void print ( short toPrint , String username )
+  {
+    print ( String.valueOf ( toPrint ) , username ) ;
+  }
+
+
   public void print ( String toPrint , String username )
   {
     boolean print = false ;
@@ -32,15 +74,21 @@ public class DebugOutPrint
   }
 
 
-  public void println ( String toPrint , String username )
+  public void println ( boolean toPrint , String username )
   {
-    print ( toPrint + "\n" , username ) ;
+    print ( String.valueOf ( toPrint ) + "\n" , username ) ;
   }
 
 
-  public void print ( int toPrint , String username )
+  public void println ( double toPrint , String username )
   {
-    print ( String.valueOf ( toPrint ) , username ) ;
+    print ( String.valueOf ( toPrint ) + "\n" , username ) ;
+  }
+
+
+  public void println ( float toPrint , String username )
+  {
+    print ( String.valueOf ( toPrint ) + "\n" , username ) ;
   }
 
 
@@ -50,18 +98,32 @@ public class DebugOutPrint
   }
 
 
-  public LinkedList < String > getUserDebug ( )
+  public void println ( long toPrint , String username )
   {
-    return this.userDebug ;
+    print ( String.valueOf ( toPrint ) + "\n" , username ) ;
   }
-  public void remove(String username)
+
+
+  public void println ( short toPrint , String username )
   {
-  	for ( int i = 0 ; i < this.userDebug.size ( ) ; i ++ )
+    print ( String.valueOf ( toPrint ) + "\n" , username ) ;
+  }
+
+
+  public void println ( String toPrint , String username )
+  {
+    print ( toPrint + "\n" , username ) ;
+  }
+
+
+  public void remove ( String username )
+  {
+    for ( int i = 0 ; i < this.userDebug.size ( ) ; i ++ )
     {
       if ( username.equalsIgnoreCase ( this.userDebug.get ( i ) ) )
       {
-        userDebug.remove(i);
-        i-- ;
+        this.userDebug.remove ( i ) ;
+        i -- ;
       }
     }
   }
