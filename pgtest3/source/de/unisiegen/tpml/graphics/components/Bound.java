bd5ee075c3860cf9dc0d2ccd67b3b9e87a84e50d
Benjamin Mies <benjamin.mies@googlemail.com>
2006-11-25 12:36:48 +0000

    documentation added
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@771 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/graphics/components/Bound.java
+++ pgtest3/source/de/unisiegen/tpml/graphics/components/Bound.java
@@ -7,12 +7,34 @@ import de.unisiegen.tpml.core.prettyprinter.PrettyAnnotation;
 
 public class Bound
 {
-	
+	/**
+	 * Startoffset of the Identifier
+	 */ 
 	private int startOffset;
+	
+	/**
+	 * Endoffset of the Identifier
+	 */
 	private int endOffset;
+	
+	/**
+	 * Identifier as String
+	 */
 	private String identifier;
+	
+	/**
+	 * Identifier as Expression
+	 */
 	private Expression expression;
+	
+	/**
+	 * List of Annotations with all Variables which are bound at the Identifier
+	 */
 	private LinkedList<PrettyAnnotation> marks = new LinkedList<PrettyAnnotation>();
+	
+	/**
+	 * List of Expressions with all Variables which are bound at the Identifier
+	 */
 	private LinkedList<Expression>expressions=new LinkedList<Expression>();
 	
 	public Bound( int start, int end, Expression pExpression,String id)
@@ -30,28 +52,65 @@ public class Bound
 		marks=list;
 	}
 
+	/**
+	 * returns the Endoffset of the Identifier
+	 * @return
+	 */
 	public int getEndOffset()
 	{
 		return endOffset;
 	}
 
+	/**
+	 * returns a list of marks of Variables
+	 * @return
+	 */
 	public LinkedList<PrettyAnnotation> getMarks()
 	{
 		return marks;
 	}
+	
+	/**
+	 * returns the PrettyAnnotation on position "index"
+	 * @param index
+	 * @return
+	 */
+	public PrettyAnnotation getMark(int index)
+	{
+		return marks.get(index);
+	}
 
+	/**
+	 * returns the Startoffset of the Identifier
+	 * @return
+	 */
 	public int getStartOffset()
 	{
 		return startOffset;
 	}
+	
+	/**
+	 * returns a list of Expressions with all Variables
+	 * @return
+	 */
 	public LinkedList<Expression> getExpressions()
 	{
 		return expressions;
 	}
+	
+	/**
+	 * returns the Identifier as Expression
+	 * @return
+	 */
 	public Expression getExpression()
 	{
 		return expression;
 	}
+	
+	/**
+	 * Returns the Identifier as String
+	 * @return
+	 */
 	public String getIdentifier()
 	{
 		return identifier;
