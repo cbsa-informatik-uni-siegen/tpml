0da72b7fc93a35bdc0ded233421322b60a1715a7
Michael Oeste <m.oeste@gmx.de>
2007-01-11 15:08:29 +0000

    kleiner Fehler behohen
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@1026 69bfd797-8304-0410-886d-9c04947c740f

--- de.unisiegen.tpml.ui/source/de/unisiegen/tpml/ui/Start.java
+++ de.unisiegen.tpml.ui/source/de/unisiegen/tpml/ui/Start.java
@@ -136,7 +136,6 @@ public class Start
 		int neededMaster = MASTERN;
 		int neededSlave = SLAVEN;
 		boolean force = false;
-		int forceInt = -1;
 
 		if (countArgs > 0)
 		{
@@ -144,7 +143,6 @@ public class Start
 			{
 				if (args[i].equalsIgnoreCase("-f"))
 				{
-					forceInt = i;
 					force = true;
 				}
 			}
