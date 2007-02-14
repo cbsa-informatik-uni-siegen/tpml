5be6205894b7c646568530fada1ce9ae21fdc653
Benjamin Mies <benjamin.mies@googlemail.com>
2007-02-14 16:34:00 +0000

    Typeinference moveon
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@1170 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest3/source-impl/de/unisiegen/tpml/core/languages/l4/L4TypeInferenceProofRuleSet.java
@@ -0,0 +1,10 @@
+package de.unisiegen.tpml.core.languages.l4;
+
+public class L4TypeInferenceProofRuleSet extends L4TypeCheckerProofRuleSet {
+
+	public L4TypeInferenceProofRuleSet(L4Language language) {
+		super(language);
+		// TODO Auto-generated constructor stub
+	}
+
+}
