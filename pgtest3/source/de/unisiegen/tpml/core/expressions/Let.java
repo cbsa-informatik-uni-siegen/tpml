6a7746108e5dd257b6f9bf6c720e3e8bfa6e8a1c
Christian Fehler <christian.fehler@gmx.de>
2007-02-20 00:04:50 +0000

    Move the break in the prettystring.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@1217 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/core/expressions/Let.java
+++ pgtest3/source/de/unisiegen/tpml/core/expressions/Let.java
@@ -243,8 +243,8 @@ public class Let extends Expression
     builder.addText ( " = " ) ; //$NON-NLS-1$
     builder.addBuilder ( this.e1
         .toPrettyStringBuilder ( pPrettyStringBuilderFactory ) , PRIO_LET_E1 ) ;
-    builder.addBreak ( ) ;
     builder.addText ( " " ) ; //$NON-NLS-1$
+    builder.addBreak ( ) ;
     builder.addKeyword ( "in" ) ; //$NON-NLS-1$
     builder.addText ( " " ) ; //$NON-NLS-1$
     builder.addBuilder ( this.e2
