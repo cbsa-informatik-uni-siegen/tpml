866415b3fa7ad56768738bc194c1a9bc26249407
Michael Oeste <m.oeste@gmx.de>
2007-05-31 14:19:42 +0000

    Pong Pimped
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@1607 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest3/source/de/unisiegen/tpml/graphics/pong/PongView.java
@@ -0,0 +1,88 @@
+package de.unisiegen.tpml.graphics.pong;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JWindow;
+
+import de.unisiegen.tpml.graphics.Messages;
+
+/**
+ * TODO Add documentation here.
+ * 
+ * @version $Rev$
+ * @author Marcell Fischbach
+ *
+ * @see de.unisiegen.tpml.graphics.pong.Pong
+ */
+public class PongView extends JWindow {
+	//
+	// Constants
+	//
+	
+	/**
+	 * The unique serialization identifier for this class.
+	 */
+	private static final long serialVersionUID = 83888488905679466L;
+	
+	
+	
+	//
+	// Constructor
+	//
+	
+	/**
+	 * TODO Add documentation here.
+	 * 
+	 * @param owner
+	 * 
+	 * @throws NullPointerException if <code>owner</code> is <code>null</code>.
+	 */
+	public PongView (Frame owner) {
+		//super(owner, true);
+		super(owner);
+		
+		if (owner == null) {
+			throw new NullPointerException("owner is null"); //$NON-NLS-1$
+		}
+		
+		//setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLayout(new BorderLayout());
+		
+		JPanel pongPanel = new JPanel(new BorderLayout());
+		pongPanel.add(new Pong(this), BorderLayout.CENTER);
+		pongPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+		add(pongPanel, BorderLayout.CENTER);
+
+		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 6, 6));
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		JButton closeButton = new JButton(Messages.getString("PongView.0")); //$NON-NLS-1$
+		closeButton.setMnemonic(Messages.getString("PongView.1").charAt(0)); //$NON-NLS-1$
+		buttonPanel.add(closeButton);
+		closeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+	
+		//setTitle("Pong"); //$NON-NLS-1$
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice gs = ge.getDefaultScreenDevice();
+		gs.setFullScreenWindow(this);
+		this.setAlwaysOnTop(true);
+    this.validate();
+		//setSize(640, 480);
+		setLocationRelativeTo(owner);
+	}
+}
