b71597041d25e67dee5f48d595d386945f1b2f21
Christian Fehler <christian.fehler@gmx.de>
2007-06-18 14:03:13 +0000

    Add the getSeenTypes method to the interface.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@1683 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/core/typeinference/TypeInferenceProofNode.java
+++ pgtest3/source/de/unisiegen/tpml/core/typeinference/TypeInferenceProofNode.java
@@ -1,154 +1,171 @@
-package de.unisiegen.tpml.core.typeinference;
+package de.unisiegen.tpml.core.typeinference ;
 
-import java.util.ArrayList;
 
-import javax.swing.tree.TreeNode;
+import java.util.ArrayList ;
+import javax.swing.tree.TreeNode ;
+import de.unisiegen.tpml.core.ProofNode ;
+import de.unisiegen.tpml.core.ProofStep ;
+import de.unisiegen.tpml.core.typechecker.DefaultTypeSubstitution ;
+import de.unisiegen.tpml.core.typechecker.SeenTypes ;
+import de.unisiegen.tpml.core.typechecker.TypeCheckerProofRule ;
 
-import de.unisiegen.tpml.core.ProofNode;
-import de.unisiegen.tpml.core.ProofStep;
-import de.unisiegen.tpml.core.typechecker.DefaultTypeSubstitution;
-import de.unisiegen.tpml.core.typechecker.TypeCheckerProofRule;
 
 /**
- * Base interface to nodes in a {@link de.unisiegen.tpml.core.typeinference.TypeInferenceProofModel}.
- *
+ * Base interface to nodes in a
+ * {@link de.unisiegen.tpml.core.typeinference.TypeInferenceProofModel}.
+ * 
  * @author Benjamin Mies
- *
  * @see de.unisiegen.tpml.core.ProofNode
  * @see de.unisiegen.tpml.core.typeinference.TypeInferenceProofModel
  */
-public interface TypeInferenceProofNode extends ProofNode {
+public interface TypeInferenceProofNode extends ProofNode
+{
+  //
+  // Accessors
+  //
+  /**
+   * Returns <code>true</code> if this node and all subnodes are finished. A
+   * node is finished if a {@link de.unisiegen.tpml.core.ProofRule} was applied
+   * and thereby a proper type is known.
+   * 
+   * @return <code>true</code> if finished.
+   */
+  public boolean isFinished ( ) ;
 
-	//
-	// Accessors
-	//
 
-	/**
-	 * Returns <code>true</code> if this node and all subnodes are finished. A node is finished if
-	 * a {@link de.unisiegen.tpml.core.ProofRule} was applied and thereby a proper type is known.
-	 * 
-	 * @return <code>true</code> if finished.
-	 */
-	public boolean isFinished ( );
+  /**
+   * Convenience wrapper for the getRule method, which returns the
+   * <code>TypeCheckerProofRule</code> that was applied to this node or
+   * <code>null</code> if no rule was applied to this node so far.
+   * 
+   * @return the type proof rule that was applied to this node, or
+   *         <code>null</code> if no rule was applied to this node.
+   * @see TypeCheckerProofRule
+   * @see de.unisiegen.tpml.core.ProofStep
+   */
+  public TypeCheckerProofRule getRule ( ) ;
 
-	/**
-	 * Convenience wrapper for the {@link ProofNode#getSteps()} method, which returns the
-	 * <code>TypeCheckerProofRule</code> that was applied to this node or <code>null</code>
-	 * if no rule was applied to this node so far.
-	 * 
-	 * @return the type proof rule that was applied to this node, or <code>null</code> if
-	 *         no rule was applied to this node.
-	 *
-	 * @see TypeCheckerProofRule
-	 * @see de.unisiegen.tpml.core.ProofStep
-	 */
-	public TypeCheckerProofRule getRule ( );
 
-	//
-	// Primitives
-	//
+  //
+  // Primitives
+  //
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getChildAt(int)
+   */
+  public TypeInferenceProofNode getChildAt ( int childIndex ) ;
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getChildAt(int)
-	 */
-	public TypeInferenceProofNode getChildAt ( int childIndex );
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getParent()
-	 */
-	public TypeInferenceProofNode getParent ( );
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getParent()
+   */
+  public TypeInferenceProofNode getParent ( ) ;
 
-	/**
-	 * get the proof steps of this node
-	 * @return ProofStep[] steps
-	 */
-	public ProofStep[] getSteps ( );
 
-	//
-	// Tree Queries
-	//
+  /**
+   * get the proof steps of this node
+   * 
+   * @return ProofStep[] steps
+   */
+  public ProofStep [ ] getSteps ( ) ;
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getParent()
-	 */
-	public TypeInferenceProofNode getRoot ( );
 
-	//
-	// Child Queries
-	//
+  //
+  // Tree Queries
+  //
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getParent()
+   */
+  public TypeInferenceProofNode getRoot ( ) ;
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getFirstChild()
-	 */
-	public TypeInferenceProofNode getFirstChild ( );
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getLastChild()
-	 */
-	public TypeInferenceProofNode getLastChild ( );
+  //
+  // Child Queries
+  //
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getFirstChild()
+   */
+  public TypeInferenceProofNode getFirstChild ( ) ;
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getChildAfter(javax.swing.tree.TreeNode)
-	 */
-	public TypeInferenceProofNode getChildAfter ( TreeNode aChild );
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getChildBefore(javax.swing.tree.TreeNode)
-	 */
-	public TypeInferenceProofNode getChildBefore ( TreeNode aChild );
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getLastChild()
+   */
+  public TypeInferenceProofNode getLastChild ( ) ;
 
-	//
-	// Leaf Queries
-	//
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getFirstLeaf()
-	 */
-	public TypeInferenceProofNode getFirstLeaf ( );
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getChildAfter(javax.swing.tree.TreeNode)
+   */
+  public TypeInferenceProofNode getChildAfter ( TreeNode aChild ) ;
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @see de.unisiegen.tpml.core.ProofNode#getLastLeaf()
-	 */
-	public TypeInferenceProofNode getLastLeaf ( );
 
-	/**
-	 * 
-	 * get the type substitution list of this node
-	 *
-	 * @return TypeSubstitutionList substitutions 
-	 */
-	public ArrayList < DefaultTypeSubstitution > getSubstitution ( );
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getChildBefore(javax.swing.tree.TreeNode)
+   */
+  public TypeInferenceProofNode getChildBefore ( TreeNode aChild ) ;
 
-	/**
-	 * get the first type formula of this type formula list
-	 *
-	 * @return TypeFormula first in list
-	 */
-	public TypeFormula getFirstFormula ( );
 
-	/**
-	 * get the type formula list of this node
-	 * 
-	 * @return ArrayList with all type formulas
-	 */
-	public ArrayList < TypeFormula > getAllFormulas ( );
+  //
+  // Leaf Queries
+  //
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getFirstLeaf()
+   */
+  public TypeInferenceProofNode getFirstLeaf ( ) ;
 
+
+  /**
+   * Returns the seenTypes.
+   * 
+   * @return The seenTypes.
+   */
+  public SeenTypes < TypeEquationTypeInference > getSeenTypes ( ) ;
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.ProofNode#getLastLeaf()
+   */
+  public TypeInferenceProofNode getLastLeaf ( ) ;
+
+
+  /**
+   * get the type substitution list of this node
+   * 
+   * @return TypeSubstitutionList substitutions
+   */
+  public ArrayList < DefaultTypeSubstitution > getSubstitution ( ) ;
+
+
+  /**
+   * get the first type formula of this type formula list
+   * 
+   * @return TypeFormula first in list
+   */
+  public TypeFormula getFirstFormula ( ) ;
+
+
+  /**
+   * get the type formula list of this node
+   * 
+   * @return ArrayList with all type formulas
+   */
+  public ArrayList < TypeFormula > getAllFormulas ( ) ;
 }
