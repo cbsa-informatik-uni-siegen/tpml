51e4b25a1cd4ca0f2eeca048d2ab6e7d22c3f555
Christian Fehler <christian.fehler@gmx.de>
2007-07-13 10:29:38 +0000

    Move the classes to pgtest3.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@1876 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest3/source/de/unisiegen/tpml/core/prettyprinter/PrettyPrintable.java
@@ -0,0 +1,25 @@
+package de.unisiegen.tpml.core.prettyprinter ;
+
+
+/**
+ * Base interface for classes whose instances can be pretty printed.
+ * 
+ * @author Benedikt Meurer
+ * @version $Rev:277 $
+ * @see de.unisiegen.tpml.core.prettyprinter.PrettyString
+ */
+public interface PrettyPrintable
+{
+  //
+  // Primitives
+  //
+  /**
+   * Returns a {@link PrettyString} that can be used to represent this printable
+   * object, and extract information about possible other printables contained
+   * within this object.
+   * 
+   * @return the pretty string for this printable.
+   * @see java.lang.Object#toString()
+   */
+  public PrettyString toPrettyString ( ) ;
+}
