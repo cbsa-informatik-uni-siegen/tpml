a5e51a12892a46cd2ef4e3546a73920b607cf3f2
Michael Oeste <m.oeste@gmx.de>
2007-07-26 20:48:01 +0000

    Code aufgeräumt, Kommentare
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2025 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/graphics/typeinference/TypeInferenceView.java
+++ pgtest3/source/de/unisiegen/tpml/graphics/typeinference/TypeInferenceView.java
@@ -1,206 +1,184 @@
-package de.unisiegen.tpml.graphics.typeinference ;
+package de.unisiegen.tpml.graphics.typeinference;
 
-
-import java.awt.Color ;
-import java.awt.GridBagConstraints ;
-import java.awt.GridBagLayout ;
-import java.awt.Insets ;
-import java.awt.event.ComponentAdapter ;
-import java.awt.event.ComponentEvent ;
-import javax.swing.JPanel ;
-import javax.swing.JScrollPane ;
-import javax.swing.JSplitPane ;
-import de.unisiegen.tpml.core.ProofGuessException ;
-import de.unisiegen.tpml.core.typeinference.TypeFormula ;
-import de.unisiegen.tpml.core.typeinference.TypeInferenceProofModel ;
-import de.unisiegen.tpml.core.typeinference.TypeInferenceProofNode ;
-import de.unisiegen.tpml.core.typeinference.TypeJudgement ;
-import de.unisiegen.tpml.graphics.AbstractProofView ;
-import de.unisiegen.tpml.graphics.outline.DefaultOutline ;
-import de.unisiegen.tpml.graphics.outline.Outline ;
-
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import de.unisiegen.tpml.core.ProofGuessException;
+import de.unisiegen.tpml.core.typeinference.TypeFormula;
+import de.unisiegen.tpml.core.typeinference.TypeInferenceProofModel;
+import de.unisiegen.tpml.core.typeinference.TypeInferenceProofNode;
+import de.unisiegen.tpml.core.typeinference.TypeJudgement;
+import de.unisiegen.tpml.graphics.AbstractProofView;
+import de.unisiegen.tpml.graphics.outline.DefaultOutline;
+import de.unisiegen.tpml.graphics.outline.Outline;
 
 /**
  * The implementation of the {@link de.unisiegen.tpml.graphics.ProofView}
- * interface for the small step interpreter user interface.
+ * interface for the typeinference interpreter user interface.
  * 
- * @author Marcell Fischbach
- * @author Benedikt Meurer
+ * @author michael
  * @author Christian Fehler
  * @version $Rev$
  * @see de.unisiegen.tpml.graphics.AbstractProofView
- * @see de.unisiegen.tpml.graphics.smallstep.SmallStepComponent
+ * @see de.unisiegen.tpml.graphics.typeinference.TypeInferenceComponent
  */
 public class TypeInferenceView extends AbstractProofView
 {
-  //
-  // Constants
-  //
-  /**
-   * The unique serialization identifier for this class.
-   */
-  private static final long serialVersionUID = - 8529052541636149376L ;
+	//
+	// Constants
+	//
+	/**
+	 * The unique serialization identifier for this class.
+	 */
+	private static final long serialVersionUID = -8529052541636149376L;
 
+	/**
+	 * The <code>JSplitPane</code> for the <code>component</code>.
+	 */
+	private JSplitPane jSplitPane;
 
-  /**
-   * The <code>JSplitPane</code> for the <code>component</code>.
-   */
-  private JSplitPane jSplitPane ;
+	/**
+	 * The {@link Outline} of this view.
+	 * 
+	 * @see #getOutline()
+	 */
+	private Outline outline;
 
+	//
+	// Attributes
+	//
+	/**
+	 * The TypeInferenceComponent.
+	 */
+	private TypeInferenceComponent component;
 
-  /**
-   * The {@link Outline} of this view.
-   * 
-   * @see #getOutline()
-   */
-  private Outline outline ;
+	/**
+	 * The scroll pane for the <code>component</code>.
+	 */
+	private JScrollPane scrollPane;
 
+	/**
+	 * The {@link TypeInferenceProofModel}.
+	 */
+	private TypeInferenceProofModel typeInferenceProofModel;
 
-  //
-  // Attributes
-  //
-  /**
-   * The small step component.
-   */
-  private TypeInferenceComponent component ;
+	//
+	// Constructor
+	//
+	/**
+	 * Allocates a new <code>TypeInferenceView</code> for the specified
+	 * <code>model</code>.
+	 * 
+	 * @param pTypeInferenceProofModel the proof model for the TypeInferenceView.
+	 * @throws NullPointerException if <code>model</code> is <code>null</code>.
+	 */
+	public TypeInferenceView (TypeInferenceProofModel pTypeInferenceProofModel)
+	{
+		if (pTypeInferenceProofModel == null)
+		{
+			throw new NullPointerException("model is null"); //$NON-NLS-1$
+		}
+		this.typeInferenceProofModel = pTypeInferenceProofModel;
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		this.jSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		this.setLayout(new GridBagLayout());
+		this.scrollPane = new JScrollPane();
+		this.component = new TypeInferenceComponent(this.typeInferenceProofModel, isAdvanced());
+		this.scrollPane.setViewportView(this.component);
+		this.scrollPane.getViewport().setBackground(Color.WHITE);
+		this.scrollPane.addComponentListener(new ComponentAdapter() {
+			@SuppressWarnings("synthetic-access")
+			@Override
+			public void componentResized(@SuppressWarnings("unused")
+			ComponentEvent event)
+			{
+				TypeInferenceView.this.component.setAvailableWidth(TypeInferenceView.this.scrollPane.getViewport()
+						.getWidth());
+			}
+		});
+		this.outline = new DefaultOutline(this);
+		TypeFormula t = ((TypeInferenceProofNode) this.typeInferenceProofModel.getRoot().getLastLeaf())
+				.getAllFormulas().get(0);
+		if (t instanceof TypeJudgement)
+		{
+			TypeJudgement typeJudgement = (TypeJudgement) t;
+			this.outline.load(typeJudgement.getExpression(), Outline.ExecuteInit.TYPEINFERENCE);
+		}
+		JPanel jPanelOutline = this.outline.getPanel();
+		jPanelOutline.getPreferredSize().getHeight();
+		this.jSplitPane.setLeftComponent(this.scrollPane);
+		this.jSplitPane.setRightComponent(jPanelOutline);
+		this.jSplitPane.setOneTouchExpandable(true);
+		this.jSplitPane.setResizeWeight(0.5);
+		gridBagConstraints.fill = GridBagConstraints.BOTH;
+		gridBagConstraints.insets = new Insets(0, 0, 0, 0);
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.weightx = 10;
+		gridBagConstraints.weighty = 10;
+		this.add(this.jSplitPane, gridBagConstraints);
+	}
 
+	/**
+	 * Returns the jSplitPane.
+	 * 
+	 * @return The jSplitPane.
+	 * @see #jSplitPane
+	 */
+	public JSplitPane getJSplitPane()
+	{
+		return this.jSplitPane;
+	}
 
-  /**
-   * The scroll pane for the <code>component</code>.
-   */
-  private JScrollPane scrollPane ;
+	/**
+	 * Returns the {@link Outline} of this view.
+	 * 
+	 * @return The {@link Outline} of this view.
+	 */
+	public Outline getOutline()
+	{
+		return this.outline;
+	}
 
+	/**
+	 * Returns the typeInferenceProofModel.
+	 * 
+	 * @return The typeInferenceProofModel.
+	 * @see #typeInferenceProofModel
+	 */
+	public TypeInferenceProofModel getTypeInferenceProofModel()
+	{
+		return this.typeInferenceProofModel;
+	}
 
-  /**
-   * The {@link TypeInferenceProofModel}.
-   */
-  private TypeInferenceProofModel typeInferenceProofModel ;
+	//
+	// Primitives
+	//
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see de.unisiegen.tpml.graphics.ProofView#guess()
+	 */
+	public void guess() throws IllegalStateException, ProofGuessException
+	{
+		this.component.guess();
+	}
 
-
-  //
-  // Constructor
-  //
-  /**
-   * Allocates a new <code>SmallStepView</code> for the specified
-   * <code>model</code>.
-   * 
-   * @param pTypeInferenceProofModel the proof model for the small step view.
-   * @throws NullPointerException if <code>model</code> is <code>null</code>.
-   */
-  // TODO TypeInferenceProofMelde oder TypeInfrecneModel
-  public TypeInferenceView ( TypeInferenceProofModel pTypeInferenceProofModel )
-  {
-    if ( pTypeInferenceProofModel == null )
-    {
-      throw new NullPointerException ( "model is null" ) ; //$NON-NLS-1$
-    }
-    this.typeInferenceProofModel = pTypeInferenceProofModel ;
-    GridBagConstraints gridBagConstraints = new GridBagConstraints ( ) ;
-    this.jSplitPane = new JSplitPane ( JSplitPane.VERTICAL_SPLIT ) ;
-    this.setLayout ( new GridBagLayout ( ) ) ;
-    this.scrollPane = new JScrollPane ( ) ;
-    // TODO advanced wieder einbauen
-    this.component = new TypeInferenceComponent ( this.typeInferenceProofModel ,
-        isAdvanced ( ) ) ;
-    this.scrollPane.setViewportView ( this.component ) ;
-    this.scrollPane.getViewport ( ).setBackground ( Color.WHITE ) ;
-    this.scrollPane.addComponentListener ( new ComponentAdapter ( )
-    {
-      @ SuppressWarnings ( "synthetic-access" )
-      @ Override
-      public void componentResized ( @ SuppressWarnings ( "unused" )
-      ComponentEvent event )
-      {
-        TypeInferenceView.this.component
-            .setAvailableWidth ( TypeInferenceView.this.scrollPane
-                .getViewport ( ).getWidth ( ) ) ;
-      }
-    } ) ;
-    this.outline = new DefaultOutline ( this ) ;
-    TypeFormula t = ( ( TypeInferenceProofNode ) this.typeInferenceProofModel
-        .getRoot ( ).getLastLeaf ( ) ).getAllFormulas ( ).get ( 0 ) ;
-    if ( t instanceof TypeJudgement )
-    {
-      TypeJudgement typeJudgement = ( TypeJudgement ) t ;
-      this.outline.load ( typeJudgement.getExpression ( ) ,
-          Outline.ExecuteInit.TYPEINFERENCE ) ;
-    }
-    JPanel jPanelOutline = this.outline.getPanel ( ) ;
-    jPanelOutline.getPreferredSize ( ).getHeight ( ) ;
-    this.jSplitPane.setLeftComponent ( this.scrollPane ) ;
-    this.jSplitPane.setRightComponent ( jPanelOutline ) ;
-    this.jSplitPane.setOneTouchExpandable ( true ) ;
-    this.jSplitPane.setResizeWeight ( 0.5 ) ;
-    gridBagConstraints.fill = GridBagConstraints.BOTH ;
-    gridBagConstraints.insets = new Insets ( 0 , 0 , 0 , 0 ) ;
-    gridBagConstraints.gridx = 0 ;
-    gridBagConstraints.gridy = 0 ;
-    gridBagConstraints.weightx = 10 ;
-    gridBagConstraints.weighty = 10 ;
-    this.add ( this.jSplitPane , gridBagConstraints ) ;
-  }
-
-
-  /**
-   * Returns the jSplitPane.
-   * 
-   * @return The jSplitPane.
-   * @see #jSplitPane
-   */
-  public JSplitPane getJSplitPane ( )
-  {
-    return this.jSplitPane ;
-  }
-
-
-  /**
-   * Returns the {@link Outline} of this view.
-   * 
-   * @return The {@link Outline} of this view.
-   */
-  public Outline getOutline ( )
-  {
-    return this.outline ;
-  }
-
-
-  /**
-   * Returns the typeInferenceProofModel.
-   * 
-   * @return The typeInferenceProofModel.
-   * @see #typeInferenceProofModel
-   */
-  public TypeInferenceProofModel getTypeInferenceProofModel ( )
-  {
-    return this.typeInferenceProofModel ;
-  }
-
-
-  //
-  // Primitives
-  //
-  /**
-   * {@inheritDoc}
-   * 
-   * @see de.unisiegen.tpml.graphics.ProofView#guess()
-   */
-  public void guess ( ) throws IllegalStateException , ProofGuessException
-  {
-    this.component.guess ( ) ;
-  }
-
-
-  /**
-   * {@inheritDoc}
-   * 
-   * @see de.unisiegen.tpml.graphics.AbstractProofView#setAdvanced(boolean)
-   */
-  @ Override
-  public void setAdvanced ( boolean advanced )
-  {
-    // TODO testaugabe System.out.println("jetzt bekommt der TypeInference-View
-    // den advaced-Wert: "+advanced);
-    super.setAdvanced ( advanced ) ;
-    this.component.setAdvanced ( isAdvanced ( ) ) ;
-  }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see de.unisiegen.tpml.graphics.AbstractProofView#setAdvanced(boolean)
+	 */
+	@Override
+	public void setAdvanced(boolean advanced)
+	{
+		super.setAdvanced(advanced);
+		this.component.setAdvanced(isAdvanced());
+	}
 }
