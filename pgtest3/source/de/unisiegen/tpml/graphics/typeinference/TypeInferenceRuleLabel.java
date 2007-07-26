55f3df978869d93b21426c17fca8f2602d55685d
Michael Oeste <m.oeste@gmx.de>
2007-07-26 21:41:51 +0000

    Code aufgeräumt und Kommentare
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2027 69bfd797-8304-0410-886d-9c04947c740f

--- pgtest3/source/de/unisiegen/tpml/graphics/typeinference/TypeInferenceRuleLabel.java
+++ pgtest3/source/de/unisiegen/tpml/graphics/typeinference/TypeInferenceRuleLabel.java
@@ -1,7 +1,6 @@
 package de.unisiegen.tpml.graphics.typeinference;
 
 import java.awt.Dimension;
-import java.awt.Font;
 
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -9,25 +8,11 @@ import javax.swing.JLabel;
 import de.unisiegen.tpml.core.expressions.Expression;
 
 /**
- * Just a simple Component combining two {@link JLabel}s together. One
- * for the name of the rules with a leading <i>(</i> and and a tailing
- * <i>)</i>. And one for a possible exponent, when the rules have been
- * grouped together. When the exponent is <i>1</i> it will not be shown.<br>
- * <br>The following example will display those two possible renderings.<br>
+ * Just a simple Component showing the name of the rule in braces.
  * <br>
- * <img src="../../../../../../images/rulelabel1.png" /><br>
- * <img src="../../../../../../images/rulelabel2.png" /><br>
- * <br>
- * But no matter if there is an exponent, that should be displayed,
- * the label for the exponent is always there. This way all rule labels
- * have an equal height. Showing here:<br>
- * <img src="../../../../../../images/rulelabel_scheme.png" /><br>
- * <br>
- * The height, this components need, is always containing the exponent
- * even if it isn't shown at all. It is easier to place them if they 
- * have a homogenouse height.
+ * It is nearly the same as the SmallStepRuleLabel but without an exponent.
  * 
- * @author marcell
+ * @author michael
  * @see de.unisiegen.tpml.graphics.TypeInference.TypeInferenceView
  * @see de.unisiegen.tpml.graphics.TypeInference.TypeInferenceComponent
  * @see de.unisiegen.tpml.graphics.TypeInference.TypeInferenceNodeComponent
@@ -39,11 +24,6 @@ public class TypeInferenceRuleLabel extends JComponent {
 	 * 
 	 */
 	private static final long serialVersionUID = 2850026835245921469L;
-
-	/**
-	 * The exponent number
-	 */
-	private int						ruleCount;
 	
 	/**
 	 * The expression that may be associated with this rule.<br>
@@ -64,7 +44,7 @@ public class TypeInferenceRuleLabel extends JComponent {
 	public TypeInferenceRuleLabel (String ruleName, int ruleCount) {
 		super ();
 		
-		this.ruleCount 	= ruleCount;
+		//this.ruleCount 	= ruleCount;
 
 		
 		setLayout (null);
@@ -73,36 +53,15 @@ public class TypeInferenceRuleLabel extends JComponent {
 		add (ruleLabel);
 		ruleLabel.setText("(" + ruleName + ")");
 		
-		JLabel exponentLabel	= new JLabel ();
-		add (exponentLabel);
-		exponentLabel.setText("" + ruleCount);
-		
-		// scale the font down to 0.75
-		Font expFont = exponentLabel.getFont();
-		expFont = expFont.deriveFont(expFont.getSize2D() * 0.75f);
-		exponentLabel.setFont(expFont);
-		
 		Dimension size = null;
-		// the exponen will be placed so that 1/2 of the size of the 
-		// exponent label will appear on top of the
+
 		Dimension ruleSize 	= ruleLabel.getPreferredSize();
-		Dimension expSize 	= exponentLabel.getPreferredSize();
-		int top = expSize.height/2;
-		
-		ruleLabel.setBounds(0, top, ruleSize.width, ruleSize.height);
-		exponentLabel.setBounds(ruleSize.width, 0, expSize.width, expSize.height);
-		
-		size = new Dimension (ruleSize.width + expSize.width, ruleSize.height + top);
+
+		ruleLabel.setBounds(0, 0, ruleSize.width, ruleSize.height);
+
+		size = new Dimension (ruleSize.width, ruleSize.height);
 		
 		ruleLabel.setVisible(true);
-		
-		if (ruleCount > 1) {
-			exponentLabel.setVisible(true);
-		}
-		else {
-			ruleSize.width -= expSize.width;
-			exponentLabel.setVisible(false);
-		}
 		
 		this.stepExpression = null;
 		
@@ -112,22 +71,15 @@ public class TypeInferenceRuleLabel extends JComponent {
 		setMaximumSize (size);
 		setPreferredSize (size);
 	}
-	
-	/**
-	 * Returns the number of the exponent
-	 * @return
-	 */
-	public int getRuleCount () {
-		return this.ruleCount;
-	}
+
 	
 	/**
 	 * Sets a new expression this rule should be associated to.
-	 * @param stepExpression
+	 * @param pStepExpression
 	 * @see #stepExpression
 	 */
-	public void setStepExpression (Expression stepExpression) {
-		this.stepExpression = stepExpression;
+	public void setStepExpression (Expression pStepExpression) {
+		this.stepExpression = pStepExpression;
 	}
 	
 	/**
