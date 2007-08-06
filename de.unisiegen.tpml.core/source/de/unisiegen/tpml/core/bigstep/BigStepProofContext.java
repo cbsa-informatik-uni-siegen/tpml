1c5d818a2e793d37508d5f3b5b0e2bb2c6432c00
Christian Fehler <christian.fehler@gmx.de>
2007-08-06 14:48:25 +0000

    Remove warnings.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2153 69bfd797-8304-0410-886d-9c04947c740f

--- de.unisiegen.tpml.core/source/de/unisiegen/tpml/core/bigstep/BigStepProofContext.java
+++ de.unisiegen.tpml.core/source/de/unisiegen/tpml/core/bigstep/BigStepProofContext.java
@@ -1,146 +1,148 @@
-package de.unisiegen.tpml.core.bigstep;
+package de.unisiegen.tpml.core.bigstep ;
 
-import de.unisiegen.tpml.core.expressions.Expression;
-import de.unisiegen.tpml.core.interpreters.Store;
+
+import de.unisiegen.tpml.core.ProofNode ;
+import de.unisiegen.tpml.core.expressions.Expression ;
+import de.unisiegen.tpml.core.interpreters.Store ;
+
 
 /**
- * Base interface for {@link de.unisiegen.tpml.core.bigstep.BigStepProofRule}s to interact with the
- * {@link de.unisiegen.tpml.core.bigstep.BigStepProofModel}s when applying a rule to a specified node
- * or updating a node after a rule application.
- *
+ * Base interface for {@link de.unisiegen.tpml.core.bigstep.BigStepProofRule}s
+ * to interact with the {@link de.unisiegen.tpml.core.bigstep.BigStepProofModel}s
+ * when applying a rule to a specified node or updating a node after a rule
+ * application.
+ * 
  * @author Benedikt Meurer
  * @version $Rev$
- * 
  * @see de.unisiegen.tpml.core.bigstep.BigStepProofRule
  */
-public interface BigStepProofContext {
+public interface BigStepProofContext
+{
   //
   // Primitives
   //
-  
   /**
-   * Adds a new {@link BigStepProofNode} for the specified <code>expression</code>
-   * as child node for the given <code>node</code>.
-   * 
+   * Adds a new {@link BigStepProofNode} for the specified
+   * <code>expression</code> as child node for the given <code>node</code>.
    * The method takes care of registering the required redo and undo actions for
-   * the {@link BigStepProofModel} associated with this proof context.
-   * 
-   * This method acts as a convenience wrapper for the extended
-   * {@link #addProofNode(BigStepProofNode, Expression, MutableStore)} method,
-   * and tries to automatically determine the appropriate store.
+   * the {@link BigStepProofModel} associated with this proof context. This
+   * method acts as a convenience wrapper for the extended
+   * {@link #addProofNode(BigStepProofNode, Expression, Store)} method, and
+   * tries to automatically determine the appropriate store.
    * 
    * @param node the parent node for the new child node.
    * @param expression the {@link Expression} for which to create a new big step
-   *                   proof node below the <code>node</code>.
-   *                   
+   *          proof node below the <code>node</code>.
    * @throws NullPointerException if either the <code>node</code> or the
-   *                              <code>expression</code> is <code>null</code>.
-   *                              
+   *           <code>expression</code> is <code>null</code>.
    * @see #addProofNode(BigStepProofNode, Expression, Store)
    */
-  public void addProofNode(BigStepProofNode node, Expression expression);
-  
+  public void addProofNode ( BigStepProofNode node , Expression expression ) ;
+
+
   /**
-   * Adds a new {@link BigStepProofNode} for the specified <code>expression</code> and
-   * <code>store</code> as child node for the given <code>node</code>.
-   * 
-   * The method takes care of registering the required redo and undo actions for
-   * the {@link BigStepProofModel} associated with this proof context.
+   * Adds a new {@link BigStepProofNode} for the specified
+   * <code>expression</code> and <code>store</code> as child node for the
+   * given <code>node</code>. The method takes care of registering the
+   * required redo and undo actions for the {@link BigStepProofModel} associated
+   * with this proof context.
    * 
    * @param node the parent node for the new child node.
    * @param expression the {@link Expression} for which to create a new big step
-   *                   proof node below the <code>node</code>.
+   *          proof node below the <code>node</code>.
    * @param store the {@link Store} for the new big step proof node.
-   * 
-   * @throws NullPointerException if either the <code>node</code>, the <code>store</code>
-   *                              or the <code>expression</code> is <code>null</code>.
-   *                              
+   * @throws NullPointerException if either the <code>node</code>, the
+   *           <code>store</code> or the <code>expression</code> is
+   *           <code>null</code>.
    * @see #addProofNode(BigStepProofNode, Expression)
    * @see Store
    */
-  public void addProofNode(BigStepProofNode node, Expression expression, Store store);
-  
+  public void addProofNode ( BigStepProofNode node , Expression expression ,
+      Store store ) ;
+
+
   /**
-   * Returns <code>true</code> if the {@link BigStepProofModel} associated with
-   * this context has memory operations enabled, <code>false</code> otherwise.
-   * 
-   * Several rules - like <b>(APP)</b> - behave differently if memory operations
-   * are enabled, so you may need to test this condition.
+   * Returns <code>true</code> if the {@link BigStepProofModel} associated
+   * with this context has memory operations enabled, <code>false</code>
+   * otherwise. Several rules - like <b>(APP)</b> - behave differently if
+   * memory operations are enabled, so you may need to test this condition.
    * 
    * @return <code>true</code> if memory operations are enabled.
    */
-  public boolean isMemoryEnabled();
-  
+  public boolean isMemoryEnabled ( ) ;
+
+
   /**
-   * Allocates a new big step proof rule of the given <code>name</code> that does nothing.
-   * This is used to implement various special cases like the <b>(HD-EMPTY)</b> and
-   * <b>(TL-EMPTY)</b> rules.
+   * Allocates a new big step proof rule of the given <code>name</code> that
+   * does nothing. This is used to implement various special cases like the
+   * <b>(HD-EMPTY)</b> and <b>(TL-EMPTY)</b> rules.
    * 
    * @param name the name of the new rule.
-   * 
    * @return the newly allocated {@link BigStepProofRule}.
-   * 
    * @throws NullPointerException if <code>name</code> is <code>null</code>.
    */
-  public BigStepProofRule newNoopRule(String name);
-  
+  public BigStepProofRule newNoopRule ( String name ) ;
+
+
   /**
-   * Changes the result of the specified <code>node</code> to the given <code>result</code>.
-   * 
-   * The method takes care of registering the required redo and undo actions for
-   * the {@link BigStepProofModel} associated with this proof context.
+   * Changes the result of the specified <code>node</code> to the given
+   * <code>result</code>. The method takes care of registering the required
+   * redo and undo actions for the {@link BigStepProofModel} associated with
+   * this proof context.
    * 
    * @param node the node, whose result to change to the <code>result</code>.
    * @param result the new result for the specified <code>node</code>.
-   * 
-   * @throws NullPointerException if either <code>node</code> is <code>null</code>.
+   * @throws NullPointerException if either <code>node</code> is
+   *           <code>null</code>.
    */
-  public void setProofNodeResult(BigStepProofNode node, BigStepProofResult result);
-  
+  public void setProofNodeResult ( BigStepProofNode node ,
+      BigStepProofResult result ) ;
+
+
   /**
    * This method is a convenience wrapper for the
-   * {@link #setProofNodeResult(BigStepProofNode, Expression, MutableStore)} method,
-   * which simply passes the {@link Store} from the specified <code>node</code> (determined
-   * using the {@link BigStepProofNode#getStore()} method) as store for the
-   * {@link BigStepProofResult}, together with the <code>value</code>.
+   * {@link #setProofNodeResult(BigStepProofNode, Expression, Store)} method,
+   * which simply passes the {@link Store} from the specified <code>node</code>
+   * (determined using the {@link BigStepProofNode#getStore()} method) as store
+   * for the {@link BigStepProofResult}, together with the <code>value</code>.
    * 
    * @param node the node, whose result to change to <code>value</code>.
    * @param value the new value for the specified <code>node</code>.
-   * 
    * @throws NullPointerException if <code>node</code> is <code>null</code>.
-   * 
    * @see BigStepProofNode#getStore()
    */
-  public void setProofNodeResult(BigStepProofNode node, Expression value);
-  
+  public void setProofNodeResult ( BigStepProofNode node , Expression value ) ;
+
+
   /**
-   * Changes the result of the specified <code>node</code> to a new big step result
-   * with the given <code>value</code> and <code>store</code>. This is a convenience
-   * wrapper for {@link #setProofNodeResult(BigStepProofNode, BigStepProofResult)}.
-   * 
-   * The method takes care of registering the required redo and undo actions for the
+   * Changes the result of the specified <code>node</code> to a new big step
+   * result with the given <code>value</code> and <code>store</code>. This
+   * is a convenience wrapper for
+   * {@link #setProofNodeResult(BigStepProofNode, BigStepProofResult)}. The
+   * method takes care of registering the required redo and undo actions for the
    * {@link BigStepProofModel} associated with this proof context.
    * 
-   * @param node the node, whose result to change to <code>value</code> and <code>store</code>.
+   * @param node the node, whose result to change to <code>value</code> and
+   *          <code>store</code>.
    * @param value the new value for the specified <code>node</code>.
    * @param store the resulting store.
-   * 
-   * @throws NullPointerException if the <code>node</code> or <code>store</code>
-   *                              is <code>null</code>
+   * @throws NullPointerException if the <code>node</code> or
+   *           <code>store</code> is <code>null</code>
    */
-  public void setProofNodeResult(BigStepProofNode node, Expression value, Store store);
-  
+  public void setProofNodeResult ( BigStepProofNode node , Expression value ,
+      Store store ) ;
+
+
   /**
    * Changes the big step proof rule of the specified <code>node</code> to the
-   * given <code>rule</code>. This affects the proof steps of the <code>node</code>
-   * (see the {@link common.ProofNode#getSteps()} method for details.
-   * 
-   * The method takes care of registering the required redo and undo actions for
-   * the {@link BigStepProofModel} associated with this proof context.
+   * given <code>rule</code>. This affects the proof steps of the
+   * <code>node</code> (see the {@link ProofNode} method for details. The
+   * method takes care of registering the required redo and undo actions for the
+   * {@link BigStepProofModel} associated with this proof context.
    * 
    * @param node the node, whose associated proof step to change to a step for
-   *             the specified <code>rule</code>.
+   *          the specified <code>rule</code>.
    * @param rule the new rule to use for the proof step for <code>node</code>.
    */
-  public void setProofNodeRule(BigStepProofNode node, BigStepProofRule rule);
+  public void setProofNodeRule ( BigStepProofNode node , BigStepProofRule rule ) ;
 }
--- de.unisiegen.tpml.core/source/de/unisiegen/tpml/core/bigstep/BigStepProofNode.java
+++ de.unisiegen.tpml.core/source/de/unisiegen/tpml/core/bigstep/BigStepProofNode.java
@@ -1,146 +1,142 @@
-package de.unisiegen.tpml.core.bigstep;
+package de.unisiegen.tpml.core.bigstep ;
 
-import javax.swing.tree.TreeNode;
 
-import de.unisiegen.tpml.core.ProofNode;
-import de.unisiegen.tpml.core.expressions.Expression;
-import de.unisiegen.tpml.core.interpreters.InterpreterProofNode;
+import javax.swing.tree.TreeNode ;
+import de.unisiegen.tpml.core.ProofNode ;
+import de.unisiegen.tpml.core.expressions.Expression ;
+import de.unisiegen.tpml.core.interpreters.InterpreterProofNode ;
+
 
 /**
- * Base interface to nodes in a {@link de.unisiegen.tpml.core.bigstep.BigStepProofModel}.
- *
+ * Base interface to nodes in a
+ * {@link de.unisiegen.tpml.core.bigstep.BigStepProofModel}.
+ * 
  * @author Benedikt Meurer
  * @version $Rev$
- *
  * @see de.unisiegen.tpml.core.interpreters.InterpreterProofNode
  */
-public interface BigStepProofNode extends InterpreterProofNode {
+public interface BigStepProofNode extends InterpreterProofNode
+{
   //
   // Accessors
   //
-  
   /**
-   * Returns <code>true</code> if the proof for the sub tree started by this node is finished, otherwise
-   * <code>false</code> is returned to indicate that one or more child nodes are still not proven.
+   * Returns <code>true</code> if the proof for the sub tree started by this
+   * node is finished, otherwise <code>false</code> is returned to indicate
+   * that one or more child nodes are still not proven.
    * 
-   * @return <code>true</code> if the proof for the sub tree, whose root is this node, is finished.
-   * 
+   * @return <code>true</code> if the proof for the sub tree, whose root is
+   *         this node, is finished.
    * @see ProofNode#isProven()
    */
-  public boolean isFinished();
-  
+  public boolean isFinished ( ) ;
+
+
   /**
-   * Returns the result of the evaluation of the expression
-   * at this node, which is <code>null</code> until the
-   * node is proven.
-   * 
-   * Once the node is proven, this may be either a value
-   * with a store or an exception with a store.
+   * Returns the result of the evaluation of the expression at this node, which
+   * is <code>null</code> until the node is proven. Once the node is proven,
+   * this may be either a value with a store or an exception with a store.
    * 
    * @return the result for this node or <code>null</code>.
-   * 
    * @see Expression#isException()
    * @see Expression#isValue()
    */
-  public BigStepProofResult getResult();
-  
+  public BigStepProofResult getResult ( ) ;
+
+
   /**
-   * Convenience wrapper for the {@link ProofNode#getSteps()} method, which returns the
-   * <code>BigStepProofRule</code> that was applied to this node or <code>null</code>
-   * if no rule was applied to this node so far.
+   * Convenience wrapper for the {@link ProofNode} method, which returns the
+   * <code>BigStepProofRule</code> that was applied to this node or
+   * <code>null</code> if no rule was applied to this node so far.
    * 
-   * @return the big step rule that was applied to this node, or <code>null</code> if
-   *         no rule was applied to this node.
-   *
+   * @return the big step rule that was applied to this node, or
+   *         <code>null</code> if no rule was applied to this node.
    * @see BigStepProofRule
    * @see de.unisiegen.tpml.core.ProofStep
    */
-  public BigStepProofRule getRule();
-  
-  
-  
+  public BigStepProofRule getRule ( ) ;
+
+
   //
   // Primitives
   //
-  
   /**
    * {@inheritDoc}
    * 
    * @see ProofNode#getChildAt(int)
    */
-  public BigStepProofNode getChildAt(int n);
-  
+  public BigStepProofNode getChildAt ( int n ) ;
+
+
   /**
    * {@inheritDoc}
-   *
-   * @see common.ProofNode#getParent()
+   * 
+   * @see ProofNode#getParent()
    */
-  public BigStepProofNode getParent();
-  
-  
-  
+  public BigStepProofNode getParent ( ) ;
+
+
   //
   // Tree Queries
   //
-  
   /**
    * {@inheritDoc}
-   *
-   * @see common.ProofNode#getRoot()
+   * 
+   * @see ProofNode#getRoot()
    */
-  public BigStepProofNode getRoot();
-
+  public BigStepProofNode getRoot ( ) ;
 
 
   //
   // Child Queries
   //
+  /**
+   * {@inheritDoc}
+   * 
+   * @see ProofNode#getFirstChild()
+   */
+  public BigStepProofNode getFirstChild ( ) ;
+
 
   /**
    * {@inheritDoc}
-   *
-   * @see common.ProofNode#getFirstChild()
+   * 
+   * @see ProofNode#getLastChild()
    */
-  public BigStepProofNode getFirstChild();
-  
-  /**
-   * {@inheritDoc}
-   *
-   * @see common.ProofNode#getLastChild()
-   */
-  public BigStepProofNode getLastChild();
-  
-  /**
-   * {@inheritDoc}
-   *
-   * @see common.ProofNode#getChildAfter(javax.swing.tree.TreeNode)
-   */
-  public BigStepProofNode getChildAfter(TreeNode aChild);
-  
-  /**
-   * {@inheritDoc}
-   *
-   * @see common.ProofNode#getChildBefore(javax.swing.tree.TreeNode)
-   */
-  public BigStepProofNode getChildBefore(TreeNode aChild);
+  public BigStepProofNode getLastChild ( ) ;
 
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see ProofNode#getChildAfter(javax.swing.tree.TreeNode)
+   */
+  public BigStepProofNode getChildAfter ( TreeNode aChild ) ;
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see ProofNode#getChildBefore(javax.swing.tree.TreeNode)
+   */
+  public BigStepProofNode getChildBefore ( TreeNode aChild ) ;
 
 
   //
   // Leaf Queries
   //
+  /**
+   * {@inheritDoc}
+   * 
+   * @see ProofNode#getFirstLeaf()
+   */
+  public BigStepProofNode getFirstLeaf ( ) ;
+
 
   /**
    * {@inheritDoc}
-   *
-   * @see common.ProofNode#getFirstLeaf()
+   * 
+   * @see ProofNode#getLastLeaf()
    */
-  public BigStepProofNode getFirstLeaf();
-  
-  /**
-   * {@inheritDoc}
-   *
-   * @see common.ProofNode#getLastLeaf()
-   */
-  public BigStepProofNode getLastLeaf();
+  public BigStepProofNode getLastLeaf ( ) ;
 }
