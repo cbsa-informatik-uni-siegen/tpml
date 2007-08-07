7fa036c40deb4f327e6c9f47188eafb286546efd
Michael Oeste <m.oeste@gmx.de>
2007-08-07 11:24:34 +0000

    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2186 69bfd797-8304-0410-886d-9c04947c740f

---
+++ pgtest3/source/de/unisiegen/tpml/ui/JOpFrame.java
@@ -0,0 +1,24 @@
+package de.unisiegen.tpml.ui;
+
+import javax.swing.JOptionPane;
+
+/**
+ * This Class is only used by the Start.java. This class displays a normal JOptionPane as Errormessage and exists
+ * the programm with errorcode 1. It is neccesary to make an own class because of the compatibility to
+ * java-versions less then 1.2. This file must be compild with source and target 1.2
+ * @author Feivel
+ * 
+ */
+public class JOpFrame
+{
+	/**
+	 * displays the Optionpane as ERROR_MESSAGE
+	 * @param title			the error titel
+	 * @param message		the error message
+	 */
+	public JOpFrame (String title, String message)
+	{
+		JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
+		System.exit(1);
+	}
+}
---
+++ pgtest3/unregister.cmd
@@ -0,0 +1,15 @@
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L0 /f
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L1 /f
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L2 /f
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L3 /f
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L4 /f
+
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L0cbn /f
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L1cbn /f
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L2cbn /f
+
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L2o /f
+@reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.L2c /f
+
+::@reg delete HKEY_CLASSES_ROOT\Applications\tpml.exe /f
+@reg delete HKEY_CURRENT_USER\Software\Classes\Applications\tpml.exe /f
