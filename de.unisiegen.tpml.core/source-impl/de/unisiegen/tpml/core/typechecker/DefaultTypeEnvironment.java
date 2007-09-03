e20871fb45b1050f5e8aa39309ab7ce4b200ab27
Christian Fehler <christian.fehler@gmx.de>
2007-09-03 08:48:54 +0000

    Add the none style color.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2459 69bfd797-8304-0410-886d-9c04947c740f

--- de.unisiegen.tpml.core/source-impl/de/unisiegen/tpml/core/typechecker/DefaultTypeEnvironment.java
+++ de.unisiegen.tpml.core/source-impl/de/unisiegen/tpml/core/typechecker/DefaultTypeEnvironment.java
@@ -7,6 +7,8 @@ import java.util.Set ;
 import java.util.TreeSet ;
 import de.unisiegen.tpml.core.expressions.Identifier ;
 import de.unisiegen.tpml.core.latex.DefaultLatexCommand ;
+import de.unisiegen.tpml.core.latex.DefaultLatexInstruction ;
+import de.unisiegen.tpml.core.latex.DefaultLatexPackage ;
 import de.unisiegen.tpml.core.latex.DefaultLatexStringBuilder ;
 import de.unisiegen.tpml.core.latex.LatexCommand ;
 import de.unisiegen.tpml.core.latex.LatexInstruction ;
@@ -129,7 +131,8 @@ public final class DefaultTypeEnvironment extends
   {
     TreeSet < LatexCommand > commands = new TreeSet < LatexCommand > ( ) ;
     commands.add ( new DefaultLatexCommand ( LATEX_TYPE_ENVIRONMENT , 1 ,
-        "[#1]" , "id1: tau1, ..., idn: taun" ) ) ; //$NON-NLS-1$ //$NON-NLS-2$
+        "\\color{" + LATEX_COLOR_NONE_STYLE + "}{[}#1\\color{" //$NON-NLS-1$ //$NON-NLS-2$
+            + LATEX_COLOR_NONE_STYLE + "}{]}" , "id1: tau1, ..., idn: taun" ) ) ; //$NON-NLS-1$//$NON-NLS-2$
     for ( Mapping < Identifier , Type > mapping : this.mappings )
     {
       for ( LatexCommand command : mapping.getSymbol ( ).getLatexCommands ( ) )
@@ -153,6 +156,9 @@ public final class DefaultTypeEnvironment extends
   public ArrayList < LatexInstruction > getLatexInstructions ( )
   {
     ArrayList < LatexInstruction > instructions = new ArrayList < LatexInstruction > ( ) ;
+    instructions.add ( new DefaultLatexInstruction ( "\\definecolor{" //$NON-NLS-1$
+        + LATEX_COLOR_NONE_STYLE + "}{rgb}{0.0,0.0,0.0}" , //$NON-NLS-1$
+        LATEX_COLOR_NONE_STYLE + ": color of normal text" ) ) ; //$NON-NLS-1$
     for ( Mapping < Identifier , Type > mapping : this.mappings )
     {
       for ( LatexInstruction instruction : mapping.getSymbol ( )
@@ -184,6 +190,7 @@ public final class DefaultTypeEnvironment extends
   public TreeSet < LatexPackage > getLatexPackages ( )
   {
     TreeSet < LatexPackage > packages = new TreeSet < LatexPackage > ( ) ;
+    packages.add ( new DefaultLatexPackage ( "color" ) ) ; //$NON-NLS-1$
     for ( Mapping < Identifier , Type > mapping : this.mappings )
     {
       for ( LatexPackage pack : mapping.getSymbol ( ).getLatexPackages ( ) )
@@ -315,8 +322,10 @@ public final class DefaultTypeEnvironment extends
       builder.addText ( LATEX_LINE_BREAK_SOURCE_CODE ) ;
       builder.addText ( DefaultLatexStringBuilder.getIndent ( pIndent
           + LATEX_INDENT ) ) ;
+      builder.addText ( "\\color{" + LATEX_COLOR_NONE_STYLE + "}{" ) ; //$NON-NLS-1$ //$NON-NLS-2$
       builder.addText ( LATEX_COLON ) ;
       builder.addText ( LATEX_SPACE ) ;
+      builder.addText ( "}" ) ; //$NON-NLS-1$
       builder.addBuilder ( this.mappings.get ( i ).getEntry ( )
           .toLatexStringBuilder ( pLatexStringBuilderFactory ,
               pIndent + LATEX_INDENT * 2 ) , 0 ) ;
@@ -325,8 +334,10 @@ public final class DefaultTypeEnvironment extends
         builder.addText ( LATEX_LINE_BREAK_SOURCE_CODE ) ;
         builder.addText ( DefaultLatexStringBuilder.getIndent ( pIndent
             + LATEX_INDENT ) ) ;
+        builder.addText ( "\\color{" + LATEX_COLOR_NONE_STYLE + "}{" ) ; //$NON-NLS-1$ //$NON-NLS-2$
         builder.addText ( LATEX_COMMA ) ;
         builder.addText ( LATEX_SPACE ) ;
+        builder.addText ( "}" ) ; //$NON-NLS-1$
       }
     }
     builder.addBuilderEnd ( ) ;
