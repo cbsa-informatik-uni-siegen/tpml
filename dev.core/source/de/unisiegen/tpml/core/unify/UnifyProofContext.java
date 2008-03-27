c0d8485bcd05d3aa8a8d685d9ebcbafc767020f4
Christian Uhrhan <chris.uhrhan@googlemail.com>
2008-03-27 10:26:51 +0000

    * Added UnifyProofContext and UnifyProofModel
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2798 69bfd797-8304-0410-886d-9c04947c740f

---
+++ dev.core/source/de/unisiegen/tpml/core/unify/UnifyProofContext.java
@@ -0,0 +1,38 @@
+package de.unisiegen.tpml.core.unify;
+
+
+import de.unisiegen.tpml.core.entities.TypeEquation;
+import de.unisiegen.tpml.core.entities.TypeEquationList;
+import de.unisiegen.tpml.core.typeinference.TypeSubstitutionList;
+
+
+/**
+ * base interface to nodes in a UnifyProofModel
+ * 
+ * @author Christian Uhrhan
+ * @version $Id: UnifyProofNode.java 2792 2008-03-14 12:25:20Z mies $
+ * @see de.unisiegen.tpml.core.unify.UnifyProofModel
+ */
+public interface UnifyProofContext
+{
+
+  /**
+   * add a type equation to the type equation list
+   * 
+   * @param eqn the equation we want to add to the list
+   */
+  public void addEquation ( TypeEquation eqn );
+
+
+  /**
+   * adds a new child node below the <code>node</code>. <code>substs</code> is
+   * the list of substition we got so far and <code>eqns</code> is the rest of
+   * the set of equations
+   * 
+   * @param node the parent node
+   * @param substs list of substitution we got so far
+   * @param eqns rest set of equations
+   */
+  public void addProofNode ( UnifyProofNode node, TypeSubstitutionList substs,
+      TypeEquationList eqns );
+}
---
+++ dev.core/source/de/unisiegen/tpml/core/unify/UnifyProofModel.java
@@ -0,0 +1,189 @@
+package de.unisiegen.tpml.core.unify;
+
+
+import de.unisiegen.tpml.core.AbstractProofModel;
+import de.unisiegen.tpml.core.AbstractProofNode;
+import de.unisiegen.tpml.core.AbstractProofRuleSet;
+import de.unisiegen.tpml.core.ProofGuessException;
+import de.unisiegen.tpml.core.ProofNode;
+import de.unisiegen.tpml.core.ProofRule;
+import de.unisiegen.tpml.core.ProofRuleException;
+import de.unisiegen.tpml.core.entities.TypeEquationList;
+import de.unisiegen.tpml.core.latex.LatexCommandList;
+import de.unisiegen.tpml.core.latex.LatexInstructionList;
+import de.unisiegen.tpml.core.latex.LatexPackageList;
+import de.unisiegen.tpml.core.latex.LatexString;
+import de.unisiegen.tpml.core.latex.LatexStringBuilder;
+import de.unisiegen.tpml.core.latex.LatexStringBuilderFactory;
+import de.unisiegen.tpml.core.prettyprinter.PrettyString;
+import de.unisiegen.tpml.core.prettyprinter.PrettyStringBuilder;
+import de.unisiegen.tpml.core.prettyprinter.PrettyStringBuilderFactory;
+import de.unisiegen.tpml.core.typeinference.TypeSubstitutionList;
+
+
+/**
+ * The heart of unification.
+ * 
+ * @author Christian Uhrhan
+ * @version $Id: UnifyProofNode.java 2792 2008-03-14 12:25:20Z mies $
+ * @see de.unisiegen.tpml.core.AbstractProofModel
+ */
+public class UnifyProofModel extends AbstractProofModel
+{
+
+  /**
+   * TODO
+   * 
+   * @param root
+   * @param ruleSet
+   */
+  protected UnifyProofModel ( AbstractProofNode root,
+      AbstractProofRuleSet ruleSet )
+  {
+    super ( root, ruleSet );
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.AbstractProofModel#guess(de.unisiegen.tpml.core.ProofNode)
+   */
+  @Override
+  public void guess ( ProofNode node ) throws ProofGuessException
+  {
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.AbstractProofModel#prove(de.unisiegen.tpml.core.ProofRule,
+   *      de.unisiegen.tpml.core.ProofNode)
+   */
+  @Override
+  public void prove ( ProofRule rule, ProofNode node )
+      throws ProofRuleException
+  {
+  }
+
+
+  /**
+   * TODO
+   * 
+   * @param node
+   * @param substs
+   * @param eqns
+   */
+  public void contextAddProofNode ( UnifyProofNode node,
+      TypeSubstitutionList substs, TypeEquationList eqns )
+  {
+
+  }
+
+
+  /**
+   * Set the actual side overlap for the exported latex file
+   * 
+   * @param overlap the side overlapping
+   * @see de.unisiegen.tpml.core.ProofModel#setOverlap(int)
+   */
+  public void setOverlap ( int overlap )
+  {
+  }
+
+
+  /**
+   * Set the number of pages for the exported latex file
+   * 
+   * @param pages number of pages
+   * @see de.unisiegen.tpml.core.ProofModel#setPages(int)
+   */
+  public void setPages ( int pages )
+  {
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.latex.LatexPrintable#getLatexCommands()
+   */
+  public LatexCommandList getLatexCommands ()
+  {
+    return null;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.latex.LatexPrintable#getLatexInstructions()
+   */
+  public LatexInstructionList getLatexInstructions ()
+  {
+    return null;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.latex.LatexPrintable#getLatexPackages()
+   */
+  public LatexPackageList getLatexPackages ()
+  {
+    return null;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.latex.LatexPrintable#toLatexString()
+   */
+  public LatexString toLatexString ()
+  {
+    return null;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @param latexStringBuilderFactory
+   * @param indent
+   * @see de.unisiegen.tpml.core.latex.LatexPrintable#toLatexStringBuilder(de.unisiegen.tpml.core.latex.LatexStringBuilderFactory,
+   *      int)
+   */
+  public LatexStringBuilder toLatexStringBuilder (
+      LatexStringBuilderFactory latexStringBuilderFactory, int indent )
+  {
+    return null;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see de.unisiegen.tpml.core.prettyprinter.PrettyPrintable#toPrettyString()
+   */
+  public PrettyString toPrettyString ()
+  {
+    return null;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @param prettyStringBuilderFactory
+   * @see de.unisiegen.tpml.core.prettyprinter.PrettyPrintable#toPrettyStringBuilder(de.unisiegen.tpml.core.prettyprinter.PrettyStringBuilderFactory)
+   */
+  public PrettyStringBuilder toPrettyStringBuilder (
+      PrettyStringBuilderFactory prettyStringBuilderFactory )
+  {
+    return null;
+  }
+
+}
