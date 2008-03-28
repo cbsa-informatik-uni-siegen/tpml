4523b325904f9671452b84fa8561a08171d0b2f2
Christian Fehler <christian.fehler@gmx.de>
2008-03-28 13:06:45 +0000

    - Remove the not needed code and start the normal version directly. - The run target must be updated to the new start class StartDev.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2800 69bfd797-8304-0410-886d-9c04947c740f

---
+++ dev.ui/source/de/unisiegen/tpml/ui/StartDev.java
@@ -0,0 +1,22 @@
+package de.unisiegen.tpml.ui;
+
+
+/**
+ * This class starts the normal version of tpml from the dev project.
+ * 
+ * @author Christian Fehler
+ * @version $Id$
+ */
+public class StartDev
+{
+
+  /**
+   * Starts the normal version.
+   * 
+   * @param arguments The command line arguments.
+   */
+  public static void main ( String arguments[] )
+  {
+    Start.main ( arguments );
+  }
+}
