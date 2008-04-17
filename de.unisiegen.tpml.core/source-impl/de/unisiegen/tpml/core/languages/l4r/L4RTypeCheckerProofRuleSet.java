45ea49e1d169e3cb72da1724ff7bc0a4c202f5c7
Christian Fehler <christian.fehler@gmx.de>
2008-04-17 13:32:49 +0000

    Instantiate the poly types in l4r.
    git-svn-id: https://vcs.informatik.uni-siegen.de/svn/tpml/trunk@2819 69bfd797-8304-0410-886d-9c04947c740f

--- de.unisiegen.tpml.core/source-impl/de/unisiegen/tpml/core/languages/l4r/L4RTypeCheckerProofRuleSet.java
+++ de.unisiegen.tpml.core/source-impl/de/unisiegen/tpml/core/languages/l4r/L4RTypeCheckerProofRuleSet.java
@@ -1,11 +1,14 @@
 package de.unisiegen.tpml.core.languages.l4r;
 
 
+import de.unisiegen.tpml.core.expressions.Constant;
 import de.unisiegen.tpml.core.languages.l1.L1Language;
 import de.unisiegen.tpml.core.languages.l2.L2Language;
 import de.unisiegen.tpml.core.languages.l3.L3Language;
 import de.unisiegen.tpml.core.languages.l4.L4Language;
 import de.unisiegen.tpml.core.languages.l4.L4TypeCheckerProofRuleSet;
+import de.unisiegen.tpml.core.typechecker.TypeCheckerProofContext;
+import de.unisiegen.tpml.core.typechecker.TypeCheckerProofNode;
 
 
 /**
@@ -29,26 +32,26 @@ public class L4RTypeCheckerProofRuleSet extends L4TypeCheckerProofRuleSet
   public L4RTypeCheckerProofRuleSet ( L4RLanguage language )
   {
     super ( language );
-    
+
     // reorganize the registered rules
-    
-    unregister ( "ABSTR"); //$NON-NLS-1$
-    unregister ( "AND"); //$NON-NLS-1$ 
-    unregister ( "APP"); //$NON-NLS-1$ 
-    unregister ( "COND"); //$NON-NLS-1$ 
-    unregister ( "OR"); //$NON-NLS-1$ 
-    unregister ( "COERCE"); //$NON-NLS-1$ 
-    unregister ( "SUBTYPE"); //$NON-NLS-1$ 
-    unregister ( "REC"); //$NON-NLS-1$ 
-    unregister ( "LIST"); //$NON-NLS-1$ 
-    unregister ( "P-CONST"); //$NON-NLS-1$ 
-    unregister ( "P-ID"); //$NON-NLS-1$ 
-    unregister ( "P-LET"); //$NON-NLS-1$ 
-    unregister ( "TUPLE"); //$NON-NLS-1$ 
-    unregister ( "COND-1"); //$NON-NLS-1$ 
-    unregister ( "SEQ"); //$NON-NLS-1$ 
-    unregister ( "WHILE"); //$NON-NLS-1$ 
-    
+
+    unregister ( "ABSTR" ); //$NON-NLS-1$
+    unregister ( "AND" ); //$NON-NLS-1$ 
+    unregister ( "APP" ); //$NON-NLS-1$ 
+    unregister ( "COND" ); //$NON-NLS-1$ 
+    unregister ( "OR" ); //$NON-NLS-1$ 
+    unregister ( "COERCE" ); //$NON-NLS-1$ 
+    unregister ( "SUBTYPE" ); //$NON-NLS-1$ 
+    unregister ( "REC" ); //$NON-NLS-1$ 
+    unregister ( "LIST" ); //$NON-NLS-1$ 
+    unregister ( "P-CONST" ); //$NON-NLS-1$ 
+    unregister ( "P-ID" ); //$NON-NLS-1$ 
+    unregister ( "P-LET" ); //$NON-NLS-1$ 
+    unregister ( "TUPLE" ); //$NON-NLS-1$ 
+    unregister ( "COND-1" ); //$NON-NLS-1$ 
+    unregister ( "SEQ" ); //$NON-NLS-1$ 
+    unregister ( "WHILE" ); //$NON-NLS-1$ 
+
     registerByMethodName ( L1Language.L1, "ABSTR", "applyAbstr" ); //$NON-NLS-1$ //$NON-NLS-2$
     registerByMethodName ( L2Language.L2, "AND", "applyAnd" );//$NON-NLS-1$ //$NON-NLS-2$
     registerByMethodName ( L1Language.L1, "APP", "applyApp" );//$NON-NLS-1$ //$NON-NLS-2$
@@ -67,4 +70,20 @@ public class L4RTypeCheckerProofRuleSet extends L4TypeCheckerProofRuleSet
     registerByMethodName ( L4Language.L4, "WHILE", "applyWhile" ); //$NON-NLS-1$ //$NON-NLS-2$
   }
 
+
+  /**
+   * Applies the <b>(CONST)</b> rule to the <code>node</code> using the
+   * <code>context</code> and instantiates poly types.
+   * 
+   * @param context the type checker proof context.
+   * @param node the type checker proof node.
+   */
+  @Override
+  public void applyConst ( TypeCheckerProofContext context,
+      TypeCheckerProofNode node )
+  {
+    Constant constant = ( Constant ) node.getExpression ();
+    context.addEquation ( node.getType (), context.instantiate ( context
+        .getTypeForExpression ( constant ) ) );
+  }
 }
